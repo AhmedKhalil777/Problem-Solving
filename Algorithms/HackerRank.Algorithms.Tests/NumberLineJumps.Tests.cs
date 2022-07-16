fac9cdebfadc9e705850bd9c890d91fd1ecd49c2
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-07-16 21:52:49 +0200

    NumberLineJumps.cs

---
+++ Algorithms/HackerRank.Algorithms.Tests/NumberLineJumps.Tests.cs
@@ -0,0 +1,17 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class NumberLineJumps
+{
+    
+    [Fact]
+    public void kangaroo_GivenKangaroosCanNotCollision_ShouldReturnNo()
+    {
+        Algorithms.NumberLineJumps.kangaroo(0, 2, 5, 3).Should().Be("NO");
+    }
+    
+    [Fact]
+    public void kangaroo_GivenKangaroosCanCollision_ShouldReturnYes()
+    {
+        Algorithms.NumberLineJumps.kangaroo(0, 3, 4, 2).Should().Be("YES");
+    }
+}
---
+++ Algorithms/HackerRank.Algorithms/NumberLineJumps.cs
@@ -0,0 +1,21 @@
+﻿namespace HackerRank.Algorithms;
+
+public static class NumberLineJumps
+{
+    public static string kangaroo(int x1, int v1, int x2, int v2)
+    {
+        bool _isNotSkipped = true;
+        while (_isNotSkipped)
+        {
+            if (x1== x2)
+            {
+                return "YES";
+            }
+            _isNotSkipped = !(v1 >= v2 && x1 > x2 || v2 >= v1 && x2 > x1);
+            x1 += v1;
+            x2 += v2;
+        }
+
+        return "NO";
+    }
+}
