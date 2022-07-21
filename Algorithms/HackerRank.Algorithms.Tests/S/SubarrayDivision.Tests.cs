bca451df7f6874b1290bc55548075f1757033451
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-07-21 18:16:25 +0200

    Subarray Division

---
+++ Algorithms/HackerRank.Algorithms.Tests/S/SubarrayDivision.Tests.cs
@@ -0,0 +1,18 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class SubarrayDivision
+{
+    [Fact]
+    public void birthday_GivenChocolateBarsAndBirth_ShouldReturnExpectedPossibilities1()
+    {
+        Algorithms.SubarrayDivision.birthday(new List<int> { 1, 2, 1, 3, 2 }, 3, 2)
+            .Should().Be(2);
+    }
+    
+    [Fact]
+    public void birthday_GivenChocolateBarsAndBirth_ShouldReturnExpectedPossibilities2()
+    {
+        Algorithms.SubarrayDivision.birthday(new List<int> { 1 ,1, 1, 1 ,1, 1 }, 3, 2)
+            .Should().Be(0);
+    }
+}
---
+++ Algorithms/HackerRank.Algorithms/S/SubarrayDivision.cs
@@ -0,0 +1,19 @@
+﻿namespace HackerRank.Algorithms;
+
+public static class SubarrayDivision
+{
+    public static int birthday(List<int> s, int d, int m)
+    {
+        // # Possibilities;
+        var p = 0;
+        var i = 0;
+        while (i + m <= s.Count)
+        {
+            var subArray = s.Skip(i).Take(m);
+            if (subArray.Sum() == d)
+                p++;
+            i++;
+        }
+        return p;
+    }
+}
