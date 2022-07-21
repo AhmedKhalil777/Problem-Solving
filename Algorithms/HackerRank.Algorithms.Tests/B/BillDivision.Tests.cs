78e1911118b96d6f2089e6d696ae478a9b0b7971
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-07-21 21:58:55 +0200

    Bon Appetit

---
+++ Algorithms/HackerRank.Algorithms.Tests/B/BillDivision.Tests.cs
@@ -0,0 +1,21 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class BillDivision
+{
+    [Fact]
+    public void bonAppetit_GivenBrianCanNotPay_ShouldPrintBonAppetit()
+    {
+        Algorithms.BillDivision
+            .bonAppetit(new List<int>{3 ,10, 2 , 9}, 1,7);
+        Algorithms.BillDivision.Result.Should().Be("Bon Appetit");
+    }
+    
+    
+    [Fact]
+    public void bonAppetit_GivenBrianCanPay_ShouldPrintBrainPocketAfterPay()
+    {
+        Algorithms.BillDivision
+            .bonAppetit(new List<int>{3, 10, 2, 9}, 1,12);
+        Algorithms.BillDivision.Result.Should().Be("5");
+    }
+}
---
+++ Algorithms/HackerRank.Algorithms/B/BillDivision.cs
@@ -0,0 +1,23 @@
+﻿namespace HackerRank.Algorithms;
+
+public class BillDivision
+{
+    /*
+ * Complete the 'bonAppetit' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY bill
+ *  2. INTEGER k
+ *  3. INTEGER b
+ */
+
+    public static string Result { get; private set; }
+
+    public static void bonAppetit(List<int> bill, int k, int b)
+    {
+        bill[k] = 0;
+        var brianPocketAfterBay =b - (bill.Sum() /2);
+        Result = brianPocketAfterBay > 0 ? $"{brianPocketAfterBay}" : "Bon Appetit";
+        Console.WriteLine(Result);
+    }
+}
