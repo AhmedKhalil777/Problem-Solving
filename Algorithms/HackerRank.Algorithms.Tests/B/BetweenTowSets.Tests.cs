380350ed2af1233a597250d40e7bf4a307bee958
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-07-21 17:34:37 +0200

    WIP

---
+++ Algorithms/HackerRank.Algorithms.Tests/B/BetweenTowSets.Tests.cs
@@ -0,0 +1,22 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class BetweenTowSets
+{
+    [Fact]
+    public void getTotalX_GivenTowSets_ShouldReturnBetweenAsExpected1()
+    {
+        Algorithms.BetweenTowSets
+            .getTotalX(new List<int> { 2, 4 }, new List<int> { 16, 32, 96 })
+            .Should()
+            .Be(3);
+    }
+    
+    [Fact]
+    public void getTotalX_GivenTowSets_ShouldReturnBetweenAsExpected2()
+    {
+        Algorithms.BetweenTowSets
+            .getTotalX(new List<int> { 3, 4 }, new List<int> { 24, 48 })
+            .Should()
+            .Be(2);
+    }
+}
---
+++ Algorithms/HackerRank.Algorithms/B/BetweenTowSets.cs
@@ -0,0 +1,43 @@
+﻿namespace HackerRank.Algorithms;
+
+public static class BetweenTowSets
+{
+    public static int getTotalX(List<int> a, List<int> b)
+    {
+        var f = Lcm(a);
+        var l = Gcd(b);
+        var count = 0;
+        for(int i = f, j =2; i<=l; i=f*j,j++){
+            if(l%i==0){ count++;}
+        }
+        return count;
+    }
+    
+    
+    private static int Gcd(int a, int b) {
+        while (b > 0) {
+            (a , b ) = (b , a % b);
+        }
+        return a;
+    }
+    
+    private static int Gcd(IReadOnlyList<int> input) {
+        int result = input[0];
+        for (int i = 1; i < input.Count; i++) {
+            result = Gcd(result, input[i]);
+        }
+        return result;
+    }
+    
+    private static int Lcm(int a, int b) {
+        return a * (b / Gcd(a, b));
+    }
+
+    private static int Lcm(IReadOnlyList<int> input) {
+        var result = input[0];
+        for (var i = 1; i < input.Count; i++) {
+            result = Lcm(result, input[i]);
+        }
+        return result;
+    }
+}
