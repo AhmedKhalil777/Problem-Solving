cb68ba3dba518333b25d02001a64659557eee141
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-07-21 22:29:49 +0200

    Sales By Match

---
+++ Algorithms/HackerRank.Algorithms.Tests/S/SalesByMatch.Tests.cs
@@ -0,0 +1,11 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class SalesByMatch
+{
+    [Fact]
+    public void sockMerchant_GivenPairsOfSocks_ShouldReturnExpectedNumOfPairs()
+    {
+        Algorithms.SalesByMatch.sockMerchant(9, new List<int>{10, 20, 20, 10, 10, 30, 50, 10, 20})
+            .Should().Be(3);
+    }
+}
---
+++ Algorithms/HackerRank.Algorithms/S/SalesByMatch.cs
@@ -0,0 +1,22 @@
+﻿namespace HackerRank.Algorithms;
+
+public static class SalesByMatch
+{
+    
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<int> ar)
+    {
+       return ar.GroupBy(x => x)
+            .Select(x => (x.Count() / 2))
+            .Sum();
+    }
+
+}
