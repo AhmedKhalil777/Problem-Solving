cd3e270b9ef997556ede327a42f92a5b6503079b
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-07-21 23:13:43 +0200

    Drawing Book

---
+++ Algorithms/HackerRank.Algorithms.Tests/D/DrawingBook.Tests.cs
@@ -0,0 +1,28 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class DrawingBook
+{
+    [Fact]
+    public void pageCount_GivenPageRequired_ShouldReturnNumberOfLeastTurns()
+    {
+        Algorithms.DrawingBook.pageCount(5,4)
+            .Should().Be(0);
+        
+        Algorithms.DrawingBook.pageCount(6,2)
+            .Should().Be(1);
+        
+        Algorithms.DrawingBook.pageCount(5,3)
+            .Should().Be(1);
+        
+        Algorithms.DrawingBook.pageCount(6,5)
+            .Should().Be(1);
+        Algorithms.DrawingBook.pageCount(7,3)
+            .Should().Be(1);
+        Algorithms.DrawingBook.pageCount(6,5)
+            .Should().Be(1);
+        
+        Algorithms.DrawingBook.pageCount(4,4)
+            .Should().Be(0);
+        
+    }
+}
---
+++ Algorithms/HackerRank.Algorithms/D/DrawingBook.cs
@@ -0,0 +1,24 @@
+﻿namespace HackerRank.Algorithms;
+
+public class DrawingBook
+{
+    /*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
+
+    public static int pageCount(int n, int p)
+    {
+        var middle = n / 2;
+        if (p <= middle)
+          return p / 2;
+        return n %2 == 0  && p % 2  == 1 ? ((n - p) / 2) +1 : (n - p) / 2;
+    }
+    
+    
+
+}
