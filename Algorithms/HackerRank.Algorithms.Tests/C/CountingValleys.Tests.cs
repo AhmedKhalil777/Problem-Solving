69be49bca73df9fca36f5616e65101f9a8528cbb
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-07-22 00:28:54 +0200

    Counting Valleys

---
+++ Algorithms/HackerRank.Algorithms.Tests/C/CountingValleys.Tests.cs
@@ -0,0 +1,13 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class CountingValleys
+{
+    [Fact]
+    public void countingValleys_GivenSteps_ShouldReturnExpectedValleys()
+    {
+        Algorithms.CountingValleys.countingValleys(8 , @"DDUUUUDD")
+            .Should().Be(1);
+        Algorithms.CountingValleys.countingValleys(8 , @"UDDDUDUU")
+            .Should().Be(1);
+    }
+}
---
+++ Algorithms/HackerRank.Algorithms/C/CountingValleys.cs
@@ -0,0 +1,38 @@
+﻿namespace HackerRank.Algorithms;
+
+public static class CountingValleys
+{
+    /*
+* Complete the 'countingValleys' function below.
+*
+* The function is expected to return an INTEGER.
+* The function accepts following parameters:
+*  1. INTEGER steps
+*  2. STRING path
+*/
+
+    public static int countingValleys(int steps, string path)
+    {
+        var seaLevel = 0;
+        var hikerLevel = 0;
+        var prevStep = 0;
+        var valleys = 0;
+        foreach (var move in path)
+        {
+            if (move == 'U')
+                hikerLevel++;
+            else
+                hikerLevel--;
+            if (prevStep >= seaLevel && hikerLevel < seaLevel)
+            {
+                valleys++;
+            }
+
+            prevStep = hikerLevel;
+        }
+
+        return valleys;
+    }
+    
+    
+}
