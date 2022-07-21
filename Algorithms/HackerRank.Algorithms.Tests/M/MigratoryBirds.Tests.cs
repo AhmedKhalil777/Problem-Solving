cdb7fabb0cb0a2c64beaef9a36e4f3839f0f6447
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-07-21 18:59:12 +0200

    Migratory Birds

---
+++ Algorithms/HackerRank.Algorithms.Tests/M/MigratoryBirds.Tests.cs
@@ -0,0 +1,22 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class MigratoryBirds
+{
+    [Fact]
+    public void migratoryBirds_GivenMigratoryIdsSerial_ShouldReturnMaxTypeMigrated()
+    {
+        Algorithms.MigratoryBirds
+            .migratoryBirds(new List<int> { 1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4 })
+            .Should()
+            .Be(3);
+    }
+    
+    [Fact]
+    public void migratoryBirds_GivenMigratoryIdsSerialAsBatch_ShouldReturnMaxTypeMigrated()
+    {
+        Algorithms.MigratoryBirds
+            .migratoryBirds(File.ReadAllText("./M/migratoryBatch.txt").Split(' ').Select(x=> int.Parse(x)).ToList())
+            .Should()
+            .Be(3);
+    }
+}
---
+++ Algorithms/HackerRank.Algorithms/M/MigratoryBirds.cs
@@ -0,0 +1,19 @@
+﻿namespace HackerRank.Algorithms;
+
+public class MigratoryBirds
+{
+    /*
+ * Complete the 'migratoryBirds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+    public static int migratoryBirds(List<int> arr)
+    {
+        var queryable = arr.GroupBy(x => x)
+            .Select(x => new Tuple<int, int>(x.Key, x.Count()));
+        var maxSum = queryable.Max(x=> x.Item2);
+        return queryable.Where(x=> x.Item2 == maxSum).Min(x=> x.Item1);
+    }
+}
