bd425e995428cd2d712c16194441d078efd01bf0
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-07-21 18:34:43 +0200

    Division Sum Pairs

---
+++ Algorithms/HackerRank.Algorithms.Tests/D/DivisibleSumPairs.Tests.cs
@@ -0,0 +1,11 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class DivisibleSumPairs
+{
+    [Fact]
+    public void divisibleSumPairs_GivenPairs_ShouldReturnCountOfDivisibleSum()
+    {
+        Algorithms.DivisibleSumPairs.divisibleSumPairs(6, 3, new List<int> { 1, 3, 2, 6, 1, 2 })
+            .Should().Be(5);
+    }
+}
---
+++ Algorithms/HackerRank.Algorithms/D/DivisibleSumPairs.cs
@@ -0,0 +1,22 @@
+﻿namespace HackerRank.Algorithms;
+
+public class DivisibleSumPairs
+{
+    public static int divisibleSumPairs(int n, int k, List<int> ar)
+    {
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i+1; j < n; j++)
+            {
+                if ((ar[i] + ar[j] ) % k == 0 )
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+}
