d9442ae519eec4fcbe7b8c1de576c672cbdb45fc
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-07-21 21:26:32 +0200

    Day Of The Programmer

---
+++ Algorithms/HackerRank.Algorithms.Tests/D/DayOfTheProgrammer.Tests.cs
@@ -0,0 +1,24 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class DayOfTheProgrammer
+{
+    [Fact]
+    public void dayOfProgrammer_GivenYears_ShouldReturnExpectedDates()
+    {
+        Algorithms.DayOfTheProgrammer.dayOfProgrammer(2017)
+            .Should().Be("13.09.2017");
+        Algorithms.DayOfTheProgrammer.dayOfProgrammer(2016)
+            .Should().Be("12.09.2016");
+        Algorithms.DayOfTheProgrammer.dayOfProgrammer(2000)
+            .Should().Be("12.09.2000");
+        Algorithms.DayOfTheProgrammer.dayOfProgrammer(1800)
+            .Should().Be("12.09.1800");
+        Algorithms.DayOfTheProgrammer.dayOfProgrammer(1918)
+            .Should().Be("26.09.1918");
+        Algorithms.DayOfTheProgrammer.dayOfProgrammer(2100)
+            .Should().Be("13.09.2100");
+        Algorithms.DayOfTheProgrammer.dayOfProgrammer(1872)
+            .Should().Be("12.09.1872");
+    }
+    
+}
---
+++ Algorithms/HackerRank.Algorithms/D/DayOfTheProgrammer.cs
@@ -0,0 +1,28 @@
+﻿namespace HackerRank.Algorithms;
+
+public class DayOfTheProgrammer
+{
+    /*
+ * Complete the 'dayOfProgrammer' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER year as parameter.
+ */
+
+    public static string dayOfProgrammer(int year)
+    {
+        var feb = year switch
+        {
+            < 1918 => IsJulianLeapYear(year) ? 29: 28,
+            > 1918 => IsGregorianLeapYear(year) ? 29 : 28,
+            _ => 15
+        };
+        return $"{256 - (feb + 215)}.09.{year}";
+    }
+
+
+    private static bool IsJulianLeapYear(int year) =>  (year % 4 == 0);
+
+    private static bool IsGregorianLeapYear(int year) => ( (year % 4 == 0)  && (year % 100 != 0))  || (year % 400 == 0 ); 
+}
+
