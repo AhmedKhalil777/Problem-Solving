5f8578be37e2510dc238f2843746720f4f9f8fcb
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-07-21 17:57:01 +0200

    Breaking The Records

---
+++ Algorithms/HackerRank.Algorithms.Tests/B/BreakingTheRecords.Tests.cs
@@ -0,0 +1,13 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class BreakingTheRecords
+{
+    [Fact]
+    public void breakingRecords_GivenRecords_ShouldReturnMaxMin()
+    {
+        Algorithms.BreakingTheRecords
+            .breakingRecords(new List<int> {10, 5 ,20 ,20 ,4, 5 ,2, 25 ,1 })
+            .Should()
+            .Equal(new List<int>{2,4});
+    }
+}
---
+++ Algorithms/HackerRank.Algorithms/B/BreakingTheRecords.cs
@@ -0,0 +1,24 @@
+﻿namespace HackerRank.Algorithms;
+
+public static class BreakingTheRecords
+{
+    public static List<int> breakingRecords(List<int> scores)
+    { 
+        int max  = scores[0] , min = scores[0] , maxSum = 0 , minSum = 0 ;
+       scores.ForEach(x =>
+       {
+           if (max < x)
+           {
+               maxSum++;
+               max = x;
+           }
+           if (min> x)
+           {
+               min = x;
+               minSum++;
+           }
+           
+       });
+       return new List<int> { maxSum, minSum};
+    }
+}
