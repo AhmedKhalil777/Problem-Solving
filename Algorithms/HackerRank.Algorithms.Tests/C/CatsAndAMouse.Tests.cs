06f9ef85782a06e82cb69f87daa9b7c009a868ac
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-07-22 17:35:51 +0200

    Cat and a mouse

---
+++ Algorithms/HackerRank.Algorithms.Tests/C/CatsAndAMouse.Tests.cs
@@ -0,0 +1,13 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class CatsAndAMouse
+{
+    [Fact]
+    public void catAndMouse_GivenDifferentPositions_ShouldReturnTheWinner()
+    {
+        Algorithms.CatsAndAMouse.catAndMouse(1,2,3)
+            .Should().Be("Cat B");
+        Algorithms.CatsAndAMouse.catAndMouse(1,3,2)
+            .Should().Be("Mouse C");
+    }
+}
---
+++ Algorithms/HackerRank.Algorithms/C/CatsAndAMouse.cs
@@ -0,0 +1,17 @@
+﻿namespace HackerRank.Algorithms;
+
+public class CatsAndAMouse
+{
+    // Complete the catAndMouse function below.
+    public static string catAndMouse(int x, int y, int z)
+    {
+        var catACatch = Math.Abs(z - x);
+        var catBCatch = Math.Abs(z - y);
+        if (catACatch < catBCatch)
+        {
+            return "Cat A";
+        } 
+        return catBCatch < catACatch ? "Cat B" : "Mouse C";
+    }
+
+}
