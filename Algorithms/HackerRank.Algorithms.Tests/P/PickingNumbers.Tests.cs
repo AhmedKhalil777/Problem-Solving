ed8bf9f4af976b14568e8f56c91ca23ca794ad99
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-07-23 00:27:16 +0200

    Picking Numbers

---
+++ Algorithms/HackerRank.Algorithms.Tests/P/PickingNumbers.Tests.cs
@@ -0,0 +1,18 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class PickingNumbers
+{
+    [Fact]
+    public void pickingNumbers_GivenListOfNums_ShouldReturnLongestStreakOfAbsoluteOnes()
+    {
+        Algorithms.PickingNumbers.pickingNumbers(new List<int>{4, 6, 5, 3, 3, 1}).Should().Be(3);
+        Algorithms.PickingNumbers.pickingNumbers(new List<int>{1, 2 ,2, 3 ,1, 2}).Should().Be(5);
+        Algorithms.PickingNumbers.pickingNumbers(new List<int>{98 ,3, 99, 1, 97, 2}).Should().Be(2);
+    }
+
+    [Fact]
+    public void pickingNumbers_GivenFileListOfNums_ShouldReturnLongestStreakOfAbsoluteOnes()
+    {
+        Algorithms.PickingNumbers.pickingNumbers("./P/numbersTest.txt".AsIntList()).Should().Be(3);
+    }
+}
---
+++ Algorithms/HackerRank.Algorithms/P/PickingNumbers.cs
@@ -0,0 +1,18 @@
+﻿namespace HackerRank.Algorithms;
+
+public class PickingNumbers
+{
+    // I think it is bad but it works
+    public static int pickingNumbers(List<int> a)
+    {
+        var streaks = new List<List<int>>();
+        for (int i = 0; i < a.Count; i++)
+        {
+            streaks.Add(a.Where(x=> x - a[i] == 1  ||  x - a[i] == 0).ToList());
+            streaks.Add(a.Where(x=> x - a[i] == -1  ||  x - a[i] == 0 ).ToList());
+        }
+
+        return streaks.Select(x => x.Count).Max();
+    }
+
+}
