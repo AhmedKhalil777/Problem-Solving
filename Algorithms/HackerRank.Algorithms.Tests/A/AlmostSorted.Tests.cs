2660f8dbcb4b9f4dfd1cf55273aa4a22fd516f09
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-07-22 09:49:22 +0200

    Almost Sorted

---
+++ Algorithms/HackerRank.Algorithms.Tests/A/AlmostSorted.Tests.cs
@@ -0,0 +1,32 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class AlmostSorted
+{
+    [Fact]
+    public void almostSorted_GivenArrCanSwapped_ShouldBeYesSwap()
+    {
+        Algorithms.AlmostSorted.almostSorted(new List<int>{4,2});
+        Algorithms.AlmostSorted.IsSorted.Should().Be("yes");
+        Algorithms.AlmostSorted.Sorting.Should().Be("swap 1 2");
+    }
+    
+    [Fact]
+    public void almostSorted_GivenArrCanNotSwapped_ShouldBeNoSwap()
+    {
+        Algorithms.AlmostSorted.almostSorted(new List<int>{3,1,2});
+        Algorithms.AlmostSorted.IsSorted.Should().Be("no");
+        Algorithms.AlmostSorted.Sorting.Should().Be(null);
+    }
+    
+    
+    [Fact]
+    public void almostSorted_GivenArrCanBeReversible_ShouldBeReversed()
+    {
+        Algorithms.AlmostSorted.almostSorted(new List<int>{1, 5,4, 3, 2, 6});
+        Algorithms.AlmostSorted.IsSorted.Should().Be("yes");
+        Algorithms.AlmostSorted.Sorting.Should().Be("reverse 2 5");
+    }
+    
+    
+    
+}
---
+++ Algorithms/HackerRank.Algorithms/A/AlmostSorted.cs
@@ -0,0 +1,73 @@
+﻿namespace HackerRank.Algorithms;
+
+public static class AlmostSorted
+{
+    public static string IsSorted { get; set; }
+    public static string? Sorting { get; set; }
+    public static void almostSorted(List<int> arr)
+    {
+        int count=0;
+        int u=0,v=0;
+        
+        // If count = 1 or 2 => can be swappable
+        // If count > 1 => can be reversible
+        for (int i = 1; i < arr.Count; i++)
+        {
+            if (arr[i - 1] <= arr[i]) continue;
+            ++count;
+            if (u == 0)
+            {
+                u=i;
+            }
+            v=i;
+        }
+
+        switch (count)
+        {
+            // Can be swapped
+            case 1:
+                switch (arr.Count)
+                {
+                    case > 2 when arr[u - 1] < arr[v + 1] && arr[v] > arr[u - 2 >= 0 ? u - 2 : 0]:
+                    case 2:
+                        Console.WriteLine("yes\nswap "+u+" "+(v+1));
+                        IsSorted = "yes";
+                        Sorting = "swap " + u + " " + (v + 1);
+                        break;
+                    default:
+                        Console.WriteLine("no");
+                        IsSorted = "no";
+                        break;
+                }
+
+                break;
+            //  swappable
+            case 2:
+                Console.WriteLine("yes\nswap "+u+" "+(v+1));
+                IsSorted = "yes";
+                Sorting = "swap " + u + " " + (v + 1);
+                break;
+            // Reversible
+            case > 1 when arr[u - 2] < arr[v] && arr[u - 1] < arr[v + 1] && v - u == count - 1:
+                Console.WriteLine("yes\nreverse "+u+" "+(v+1));
+                IsSorted = "yes";
+                Sorting = "reverse " + u + " " + (v + 1);
+                break;
+            // not reversible not swappable
+            case > 1:
+                Console.WriteLine("no");
+                IsSorted = "no";
+                break;
+            // Already sorted
+            case 0:
+                Console.WriteLine("yes");
+                IsSorted = "yes";
+                break;
+        }
+    }
+    
+    
+    
+    
+    
+}
