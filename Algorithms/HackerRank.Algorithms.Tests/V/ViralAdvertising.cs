74feef783aa0a3bc95885599ac522f6f09e30a75
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-07-23 15:04:27 +0200

    #1 Viral Advertising

---
+++ Algorithms/HackerRank.Algorithms.Tests/V/ViralAdvertising.cs
@@ -0,0 +1,11 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class ViralAdvertising
+{
+    [Fact]
+    public void viralAdvertising_GiverNDays_ShouldReturnCumulativeLikes()
+    {
+        Algorithms.ViralAdvertising.viralAdvertising(5).Should().Be(24);
+        Algorithms.ViralAdvertising.viralAdvertising(3).Should().Be(9);
+    }
+}
---
+++ Algorithms/HackerRank.Algorithms/V/ViralAdvertising.cs
@@ -0,0 +1,25 @@
+﻿namespace HackerRank.Algorithms;
+
+public class ViralAdvertising
+{
+    /*
+ * Complete the 'viralAdvertising' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+    public static int viralAdvertising(int n)
+    {
+        var people = 5.0;
+        var cum = 0.0;
+        for (var i = 0; i < n; i++)
+        {
+            var liked = Math.Floor(people / 2);
+            cum += liked;
+            people = liked * 3;
+        }
+
+        return (int) cum;
+    }
+}
