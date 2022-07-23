cc2bc4e2558efa916a234305a4c053bdb5f8b28a
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-07-23 14:40:49 +0200

    Beautiful Days at the Movies

---
+++ Algorithms/HackerRank.Algorithms.Tests/B/BeautifulDaysAtTheMovies.Tests.cs
@@ -0,0 +1,17 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class BeautifulDaysAtTheMovies
+{
+    [Fact]
+    public void ReverseNum_GivenANum_ShouldReturnReverse()
+    {
+        Algorithms.BeautifulDaysAtTheMovies.ReverseNum(21).Should().Be(12);
+        Algorithms.BeautifulDaysAtTheMovies.ReverseNum(120).Should().Be(21);
+    }
+    
+    [Fact]
+    public void beautifulDays_GivenFirstAndLastAndK_ShouldReturnBeautifulDays()
+    {
+        Algorithms.BeautifulDaysAtTheMovies.beautifulDays(20 ,23, 6).Should().Be(2);
+    }
+}
---
+++ Algorithms/HackerRank.Algorithms/B/BeautifulDaysAtTheMovies.cs
@@ -0,0 +1,41 @@
+﻿namespace HackerRank.Algorithms;
+
+public class BeautifulDaysAtTheMovies
+{
+    /*
+ * Complete the 'beautifulDays' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER i
+ *  2. INTEGER j
+ *  3. INTEGER k
+ */
+
+    public static int beautifulDays(int i, int j, int k)
+    {
+        var res = 0;
+        for ( ; i <= j; i++)
+        {
+            var reverse = ReverseNum(i);
+            if ((Math.Abs(reverse - i) % k) == 0)
+                res++;
+        }
+        return res;
+    }
+
+
+    public static int ReverseNum(int num)
+    {
+        var numStr = num.ToString().ToList();
+        var reverser = 1;
+        var res = 0;
+        for (var i = 0; i < numStr.Count(); i++)
+        {
+            res += int.Parse(numStr[i].ToString()) * reverser; 
+            reverser *= 10;
+        }
+
+        return res;
+    }
+}
