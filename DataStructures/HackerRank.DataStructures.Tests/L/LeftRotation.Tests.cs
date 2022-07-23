1552d5020567389e7daf74fae77f34716a982340
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-07-23 22:15:03 +0200

    Left Rotation - Data Structures #5

---
+++ DataStructures/HackerRank.DataStructures.Tests/L/LeftRotation.Tests.cs
@@ -0,0 +1,13 @@
+﻿using FluentAssertions;
+
+namespace HackerRank.DataStructures.Tests;
+
+public class LeftRotation
+{
+    [Fact]
+    public void rotateLeft_GivenListsAndSteps_ShouldBeRotatedAsExpected()
+    {
+        DataStructures.LeftRotation.rotateLeft(4, "1 2 3 4 5".AsStringIntList())
+            .Should().Equal("5 1 2 3 4".AsStringIntList());
+    }
+}
---
+++ DataStructures/HackerRank.DataStructures/L/LeftRotation.cs
@@ -0,0 +1,18 @@
+﻿namespace HackerRank.DataStructures;
+
+public class LeftRotation
+{
+    public static List<int> rotateLeft(int d, List<int> arr)
+    {
+        var res = new int[arr.Count];
+        for (int i = 0; i < arr.Count; i++)
+        {
+            var rIndex = i - d;
+            if (rIndex < 0)
+                rIndex  += arr.Count;
+            
+            res[rIndex] = arr[i];
+        }
+        return res.ToList();
+    }
+}
