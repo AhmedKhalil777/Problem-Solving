0ef39f52666f1992965c9fffa546c3d04362b48f
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-07-23 10:35:12 +0200

    Utopian Tree

--- Algorithms/HackerRank.Algorithms.Tests/U/UtopianTree.Tests.cs
+++ Algorithms/HackerRank.Algorithms.Tests/U/UtopianTree.Tests.cs
@@ -2,5 +2,11 @@
 
 public class UtopianTree
 {
-    
+    [Fact]
+    public void utopianTree_GivenNOfCycles_ShouldReturnTreeHeight()
+    {
+        Algorithms.UtopianTree.utopianTree(0).Should().Be(1);
+        Algorithms.UtopianTree.utopianTree(1).Should().Be(2);
+        Algorithms.UtopianTree.utopianTree(4).Should().Be(7);
+    }
 }
---
+++ Algorithms/HackerRank.Algorithms/U/UtopianTree.cs
@@ -0,0 +1,20 @@
+﻿namespace HackerRank.Algorithms;
+
+public class UtopianTree
+{
+    public static int utopianTree(int n)
+    {
+        var height = 1;
+        for (var i = 1; i <= n; i++)
+        {
+            if (i % 2 == 1)
+            {
+                height += height;
+                continue;
+            }
+            height++;
+        }
+        return height;
+    }
+
+}
