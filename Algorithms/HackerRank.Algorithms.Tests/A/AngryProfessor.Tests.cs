ab1f93b91e807ca9bf7278876dae53f68b94d920
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-07-23 11:12:20 +0200

    Angry Professor

---
+++ Algorithms/HackerRank.Algorithms.Tests/A/AngryProfessor.Tests.cs
@@ -0,0 +1,11 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class AngryProfessor
+{
+    [Fact]
+    public void angryProfessor_GivenArrivalAttendees_ShouldReturnProfessorReply()
+    {
+        Algorithms.AngryProfessor.angryProfessor(3, "-1 -3 4 2".AsStringIntList()).Should().Be("YES");
+        Algorithms.AngryProfessor.angryProfessor(2, "0 -1 2 1".AsStringIntList()).Should().Be("NO");
+    }
+}
---
+++ Algorithms/HackerRank.Algorithms/A/AngryProfessor.cs
@@ -0,0 +1,20 @@
+﻿namespace HackerRank.Algorithms;
+
+public class AngryProfessor
+{
+    /*
+* Complete the 'angryProfessor' function below.
+*
+* The function is expected to return a STRING.
+* The function accepts following parameters:
+*  1. INTEGER k
+*  2. INTEGER_ARRAY a
+*/
+
+    public static string angryProfessor(int k, List<int> a)
+    {
+        var sum = Math.Abs(a.Count(x => x <= 0));
+        return (sum >= k) ? "NO" : "YES";
+
+    }
+}
