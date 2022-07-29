3d11bdc2fcd798967bb4302b8f9199defb31d188
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-07-29 08:36:29 +0200

    Print the Elements of a Linked List - DS #8

---
+++ DataStructures/HackerRank.DataStructures.Tests/P/PrintElementsOfLinkedList.Tests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+
+namespace HackerRank.DataStructures.Tests;
+
+public class PrintElementsOfLinkedList
+{
+
+    [Fact]
+    public void printLinkedList_GivenASinglyLinkedList_ShouldPrintAllItems()
+    {
+        // Arrange 
+        var linkedList = new SinglyLinkedListNode
+        {
+            data = "A",
+            next = new SinglyLinkedListNode
+            {
+                data = "B",
+                next = new SinglyLinkedListNode
+                {
+                    data = "C",
+                }
+            }
+        };
+        
+        // Act
+        DataStructures.PrintElementsOfLinkedList.printLinkedList(linkedList);
+        
+        // Assert
+        DataStructures.PrintElementsOfLinkedList.results.Should().Equal(new List<string> { "A", "B", "C" });
+    }
+}
---
+++ DataStructures/HackerRank.DataStructures/P/PrintElementsOfLinkedList.cs
@@ -0,0 +1,41 @@
+﻿namespace HackerRank.DataStructures;
+
+public class PrintElementsOfLinkedList
+{
+
+    public static List<string> results;
+    /*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+    public static void printLinkedList(SinglyLinkedListNode head)
+    {
+        // For Testing
+        results = new List<string>();
+        while (head is not null)
+        {
+            Console.WriteLine(head.data);
+            results.Add(head.data);
+            if (head.next is  null)
+            {
+                return;
+            }
+            head = head.next;
+            
+            // For Testing
+        }
+    }
+
+
+}
+
+public class SinglyLinkedListNode
+{
+    public SinglyLinkedListNode? next { get; set; }
+    public string data { get; set; }
+}
