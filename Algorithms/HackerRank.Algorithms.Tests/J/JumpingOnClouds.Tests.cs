39f0df33fafdfafaa45d02e633565d0291011a32
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-09-24 10:42:01 +0200

    Algorithms - Jumping on Clouds #12

---
+++ Algorithms/HackerRank.Algorithms.Tests/J/JumpingOnClouds.Tests.cs
@@ -0,0 +1,15 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class JumbingOnClouds
+{
+    [Fact]
+    public void JumbingOnClouds_GivenMultSet_ShouldReturnExpectedResults()
+    {
+        Algorithms.JumpingOnClouds.jumpingOnClouds(new int[] { 0, 0, 1, 0, 0, 1, 1, 0 }, 2)
+            .Should().Be(92);
+        Algorithms.JumpingOnClouds.jumpingOnClouds(new int[] { 0, 0, 1, 0 }, 2)
+        .Should().Be(96);     
+        Algorithms.JumpingOnClouds.jumpingOnClouds(new int[] { 1 ,1 ,1 ,0 ,1 ,1 ,0, 0, 0, 0 }, 3)
+        .Should().Be(80);
+    }
+}
---
+++ Algorithms/HackerRank.Algorithms/J/JumpingOnClouds.cs
@@ -0,0 +1,20 @@
+﻿namespace HackerRank.Algorithms;
+
+public static class JumpingOnClouds
+{
+    // Complete the jumpingOnClouds function below.
+    public static int jumpingOnClouds(int[] c, int k)
+    {
+        int e = 100, jump = 0;
+        while (true)
+        {
+            e--;
+            if (c[jump] == 1) e -= 2;
+            jump = (jump + k) % c.Length;
+            if (jump == 0) break;
+        }
+        return e;
+
+    }
+
+}
