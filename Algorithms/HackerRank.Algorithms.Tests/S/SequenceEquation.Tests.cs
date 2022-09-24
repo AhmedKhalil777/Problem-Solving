b47e0c1670fccbe328a4e019055b2f05a842dec2
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-09-24 09:18:45 +0200

    Algorithms - Sequence Equation #11

---
+++ Algorithms/HackerRank.Algorithms.Tests/S/SequenceEquation.Tests.cs
@@ -0,0 +1,16 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class SequenceEquation
+{
+
+    [Fact]
+    public void SequanceEquation_GivenMultiSet_ShouldReturnExpectedResults()
+    {
+        Algorithms.SequenceEquation
+            .permutationEquation(new List<int> { 5, 2, 1, 3, 4 })
+            .Should().Equal(new List<int> { 4, 2, 5, 1, 3 });
+        Algorithms.SequenceEquation
+            .permutationEquation(new List<int> { 2, 3, 1 })
+            .Should().Equal(new List<int> { 2, 3, 1 });
+    }
+}
---
+++ Algorithms/HackerRank.Algorithms/S/SequenceEquation.cs
@@ -0,0 +1,18 @@
+﻿namespace HackerRank.Algorithms;
+
+public class SequenceEquation
+{
+
+    public static List<int> permutationEquation(List<int> p)
+    {
+        var res = new List<int>();
+        for (int i = 1; i <= p.Count; i++)
+        {
+          var index =  p.FindIndex(x => x == i) + 1;
+          var permut = p.FindIndex(x=> x == index) + 1;
+            res.Add(permut);
+        }
+        return res;
+    }
+
+}
