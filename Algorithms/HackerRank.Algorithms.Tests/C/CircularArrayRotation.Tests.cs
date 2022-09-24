8c2f4a13fcfac5fc7d3f5952ee3c05ca3725061e
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-09-24 08:40:07 +0200

    Algorithms - Circular Array Rotation #10

---
+++ Algorithms/HackerRank.Algorithms.Tests/C/CircularArrayRotation.Tests.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace HackerRank.Algorithms.Tests;
+
+public class CircularArrayRotation
+{
+    [Fact]
+    public void CircularArrayRotation_GiveMultiSetOfInputs_ShoudReturnExpectedResults()
+    {
+        Algorithms.CircularArrayRotation
+            .circularArrayRotation(new List<int> { 3, 4, 5 }, 2, new List<int> { 1, 2 })
+            .Should().Equal(new List<int> { 5, 3 });
+    }
+
+    [Fact]
+    public void CircularArrayRotation_GiveBatchOfInputs_ShoudReturnExpectedResults()
+    {
+        var input = "./C/CircularArrayRotationBatch.txt".ReadLinesAsMatrix();
+        var k = input[0][1];
+        var a = input[1];
+        var q = input.Skip(2).Take(100).SelectMany(x => x).ToList();
+        var expected = input.Skip(102).Take(100).SelectMany(x => x).ToList();
+        Algorithms.CircularArrayRotation
+            .circularArrayRotation(a, k, q)
+            .Should().Equal(expected);
+    }
+}
---
+++ Algorithms/HackerRank.Algorithms/C/CircularArrayRotation.cs
@@ -0,0 +1,20 @@
+﻿namespace HackerRank.Algorithms;
+
+public static class CircularArrayRotation
+{
+
+    public static List<int> circularArrayRotation(List<int> a, int k, List<int> queries)
+    {
+        var aLen = a.Count;
+        k %= aLen;
+        return queries.Select(q =>
+        {
+            var index = q - k;
+            if (index < 0)
+            {
+                index += aLen;
+            }
+            return a[index];
+        }).ToList();
+    }
+}
