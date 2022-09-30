f48332fa9b21cd29cbdf0d1ad7e987144cf3034a
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-09-30 18:07:35 +0200

    Extra Long Factorials - Algorithms #15

---
+++ Algorithms/HackerRank.Algorithms.Tests/E/ExtraLongFactorials.Tests.cs
@@ -0,0 +1,11 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class ExtraLongFactorials
+{
+    [Fact]
+    public void ExtraLongFactorials_GivenMultiSet_ShouldReturnExpected()
+    {
+        Algorithms.ExtraLongFactorials.extraLongFactorials(30).Should().Be("265252859812191058636308480000000");
+        Algorithms.ExtraLongFactorials.extraLongFactorials(25).Should().Be("15511210043330985984000000");
+    }
+}
---
+++ Algorithms/HackerRank.Algorithms/E/ExtraLongFactorials.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+namespace HackerRank.Algorithms;
+
+public static class ExtraLongFactorials
+{
+    /*
+ * Complete the 'extraLongFactorials' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+    public static string extraLongFactorials(int n)
+    {
+        BigInteger bigInt = n;
+        while (--n > 0)
+        {
+            bigInt *= n;
+        }
+        Console.WriteLine(bigInt);
+        return bigInt.ToString();
+    }
+}
