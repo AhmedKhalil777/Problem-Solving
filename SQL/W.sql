093109a17a06ea0fec6cd8e1e980888169bdc698
Ahmed khalil <38745087+AhmedKhalil777@users.noreply.github.com>
2022-10-01 13:29:03 +0200

    Weather Observation Station 3

--- SQL/W.sql
+++ SQL/W.sql
@@ -1,3 +1,6 @@
 -- Weather Observation Station 1
 Select CITY, STATE From STATION WHERE LAT_N >= 0 AND LONG_W >= 0;
 
+-- Weather Observation Station 3
+Select DISTINCT CITY From STATION WHERE ID % 2 = 0;
+
