e26385f122dddd0b965fdf4c4dc61d8b77a21403
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-10-01 12:56:03 +0200

    Repeated String - Algorithms #21

---
+++ Algorithms/HackerRank.Algorithms.Tests/R/RepeatedString.Tests.cs
@@ -0,0 +1,11 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class RepeatedString
+{
+    [Fact]
+    public void repeatedString_GivenMultiSet_ShouldReturnExpected()
+    {
+        Algorithms.RepeatedString.repeatedString("aba",10).Should().Be(7);
+        Algorithms.RepeatedString.repeatedString("a", 1000000000000).Should().Be(1000000000000);
+    }
+}
---
+++ Algorithms/HackerRank.Algorithms/R/RepeatedString.cs
@@ -0,0 +1,34 @@
+﻿namespace HackerRank.Algorithms;
+
+public static class RepeatedString
+{
+    /*
+ * Complete the 'repeatedString' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. LONG_INTEGER n
+ */
+
+    public static long repeatedString(string s, long n)
+    {
+        var ch = 'a';
+        if (!s.Any(x=> x == ch))
+        {
+            return 0;
+        }
+        var count = s.Count(x => x == ch);
+        long result = 0;
+        long repeatings =  (n / s.Length);
+        result += (count * repeatings);
+        var remaining = n % s.Length;
+        if (remaining == 0 )
+        {
+            return result;
+        }
+        var substr = s.Substring(0, (int)remaining);
+        result += substr.Count(x => x == ch);
+        return result;
+    }
+}
