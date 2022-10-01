e1d0a873b72e6090638b6194941077de2115f65e
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-10-01 07:22:35 +0200

    Append And Delete - Algorithms #16

---
+++ Algorithms/HackerRank.Algorithms.Tests/A/AppendAndDelete.Tests.cs
@@ -0,0 +1,12 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class AppendAndDelete
+{
+    [Fact]
+    public void AppendAndDelete_GivenMultiSet_ShouldReturnExpected()
+    {
+        Algorithms.AppendAndDelete.appendAndDelete("hackerhappy", "hackerrank", 9).Should().Be("Yes");
+        Algorithms.AppendAndDelete.appendAndDelete("aba", "aba", 7).Should().Be("Yes");
+        Algorithms.AppendAndDelete.appendAndDelete("ashley", "ash", 2).Should().Be("No");
+    }
+}
---
+++ Algorithms/HackerRank.Algorithms/A/AppendAndDelete.cs
@@ -0,0 +1,59 @@
+﻿namespace HackerRank.Algorithms;
+
+public class AppendAndDelete
+{
+    /*
+ * Complete the 'appendAndDelete' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. STRING t
+ *  3. INTEGER k
+ */
+
+    public static string appendAndDelete(string s, string t, int k)
+    {
+        String result = "No";
+        if (s==t)
+        {
+           return "Yes";
+        }
+        else
+        {
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i < t.Length)
+                {
+                    if (s[i] == t[i])
+                        count++;
+                    else
+                        break;
+                }
+            }
+            int left = s.Length - count;
+            int need = t.Length - count;
+
+            if (k >= s.Length + t.Length)
+            {
+                result = "Yes";
+            }
+            else if (k >= (left + need))
+            {
+                if ((need + left) % 2 != 0)
+                {
+                    if (k % 2 != 0)
+                        result = "Yes";
+                }
+                else
+                {
+                    if (k % 2 == 0)
+                        result = "Yes";
+                }
+            }
+        }
+        return result;
+    }
+
+}
