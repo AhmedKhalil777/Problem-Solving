8e9c0df001424c32bec458169965455ce71e1c8b
Ahmed khalil <38745087+AhmedKhalil777@users.noreply.github.com>
2022-10-01 13:07:39 +0200

    Revising the Select Query I

---
+++ SQL/R.sql
@@ -0,0 +1,11 @@
+-- Revising the Select Query I
+-- /*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+
+Select * From City
+Where population >  100000  and CountryCode = 'USA'
+
+
