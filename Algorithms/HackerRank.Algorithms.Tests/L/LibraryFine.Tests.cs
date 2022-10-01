7f3d68691c79b9c90c93d1c87dc7fe6ee88bf343
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-10-01 09:50:52 +0200

    Library Fine - Algorithms #18

---
+++ Algorithms/HackerRank.Algorithms.Tests/L/LibraryFine.Tests.cs
@@ -0,0 +1,12 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class LibraryFine
+{
+    [Fact]
+    public void LibraryFine_GivenMultiSetOfDates_ShouldReturnExpectedFines()
+    {
+        Algorithms.LibraryFine.libraryFine(d1: 9, m1: 6, y1: 2015, d2: 6, m2: 6, y2: 2015).Should().Be(45);
+        Algorithms.LibraryFine.libraryFine(d1: 14, m1: 7, y1: 2018, d2: 5, m2: 7, y2: 2018).Should().Be(135);
+        Algorithms.LibraryFine.libraryFine(d1: 2, m1: 7, y1: 1014, d2: 1, m2: 1, y2: 1014).Should().Be(3000);
+    }
+}
---
+++ Algorithms/HackerRank.Algorithms/L/LibraryFine.cs
@@ -0,0 +1,28 @@
+﻿namespace HackerRank.Algorithms;
+
+public static class LibraryFine
+{
+    /*
+ * Complete the 'libraryFine' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER d1
+ *  2. INTEGER m1
+ *  3. INTEGER y1
+ *  4. INTEGER d2
+ *  5. INTEGER m2
+ *  6. INTEGER y2
+ */
+
+    public static int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2)
+    {
+        var actualDateTime = new DateTime(y1, m1, d1);
+        var expectedDateTime = new DateTime(y2, m2, d2);
+        if (actualDateTime <= expectedDateTime) return 0;
+        else if (y2 == y1 && m2 == m1) return (d1 - d2) * 15;
+        else if (y2 == y1) return (m1 - m2) * 500;
+        else return 10000;
+        
+    }
+}
