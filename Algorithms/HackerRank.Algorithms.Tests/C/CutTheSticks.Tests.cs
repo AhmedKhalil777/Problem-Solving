3d170c9dae5ee275e63cba42d10a9414a4826232
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-10-01 10:16:51 +0200

    Cut the sticks - Algorithms #19

---
+++ Algorithms/HackerRank.Algorithms.Tests/C/CutTheSticks.Tests.cs
@@ -0,0 +1,11 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class CutTheSticks
+{
+    [Fact]
+    public void CutTheSticks_GivenMultiSetOfSticks_ShouldReturnNumOfPieces()
+    {
+        Algorithms.CutTheSticks.cutTheSticks(new List<int> { 5, 4, 4, 2, 2, 8 }).Should().Equal(new List<int> { 6, 4, 2, 1});
+        Algorithms.CutTheSticks.cutTheSticks(new List<int> { 1, 2 ,3, 4, 3, 3, 2, 1 }).Should().Equal(new List<int> { 8,6,4,1});
+    }
+}
---
+++ Algorithms/HackerRank.Algorithms/C/CutTheSticks.cs
@@ -0,0 +1,26 @@
+﻿namespace HackerRank.Algorithms;
+
+public class CutTheSticks
+{
+
+    /*
+ * Complete the 'cutTheSticks' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+    public static List<int> cutTheSticks(List<int> arr)
+    {
+        var output = new List<int>(); 
+        while(arr.Any(x => x > 0))
+        {
+            arr= arr.Where(x => x > 0).ToList();
+            var min = arr.Min();
+            arr = arr.Select(x => x - min).ToList();
+            output.Add(arr.Count);
+        }
+        return output;
+    }
+
+}
