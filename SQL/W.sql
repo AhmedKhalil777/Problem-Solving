ab93d77ea7efd68e1656293af187e0a296c59339
Ahmed khalil <38745087+AhmedKhalil777@users.noreply.github.com>
2022-10-01 15:17:14 +0200

    Weather Observation Station 4

--- SQL/W.sql
+++ SQL/W.sql
@@ -4,3 +4,7 @@ Select CITY, STATE From STATION WHERE LAT_N >= 0 AND LONG_W >= 0;
 -- Weather Observation Station 3
 Select DISTINCT CITY From STATION WHERE ID % 2 = 0;
 
+-- Weather Observation Station 4
+
+Select Count(*) - Count(DISTINCT CITY)  From STATION
+
