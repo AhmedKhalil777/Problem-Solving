2a758c3d72d65fc8a396f97b3fcfb54124371a43
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-10-01 09:28:31 +0200

    Sherlock and Squares - Algorithms #17

---
+++ Algorithms/HackerRank.Algorithms.Tests/S/SherlockAndSquares.Tests.cs
@@ -0,0 +1,11 @@
+﻿namespace HackerRank.Algorithms.Tests;
+
+public class SherlockAndSquares
+{
+    [Fact]
+    public void SherlockAndSquares_GivenMultiSet_ShouldRetunNumOfSquares()
+    {
+        Algorithms.SherlockAndSquares.squares(3,9).Should().Be(2);
+        Algorithms.SherlockAndSquares.squares(17,24).Should().Be(0);
+    }
+}
---
+++ Algorithms/HackerRank.Algorithms/S/SherlockAndSquares.cs
@@ -0,0 +1,28 @@
+﻿namespace HackerRank.Algorithms;
+
+public static class SherlockAndSquares
+{
+    /*
+ * Complete the 'squares' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER a
+ *  2. INTEGER b
+ */
+
+    public static int squares(int a, int b)
+    {
+        var aSqrt = (int) Math.Sqrt(a);
+        var i = 0;
+        while(Math.Pow(aSqrt,2) <= b)
+        {
+            if (Math.Pow(aSqrt,2) >= a )
+            {
+                i++;
+            }
+            aSqrt++;
+        }
+        return i;
+    }
+}
