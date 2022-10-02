8bd636ad6649c59438a9ae24f8ad452cb295fce5
Ahmed khalil <38745087+AhmedKhalil777@users.noreply.github.com>
2022-10-02 20:03:00 +0200

    Weather Observation Station 5

--- SQL/W.sql
+++ SQL/W.sql
@@ -8,3 +8,13 @@ Select DISTINCT CITY From STATION WHERE ID % 2 = 0;
 
 Select Count(*) - Count(DISTINCT CITY)  From STATION
 
+-- Weather Observation Station 5
+
+Select TOP 1 CITY AS NAME , LEN(CITY) AS LENG
+From STATION
+Order BY LEN(CITY), CITY
+
+Select TOP 1 CITY AS NAME , LEN(CITY) AS LENG
+From STATION
+Order BY LEN(CITY) DESC , CITY
+
