c54de8ebd61ea3127bbc89fb47f408b6960116ab
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-10-07 18:21:29 +0200

    Matching Digits Vs Non Digits

---
+++ Regex/Introduction/MatchingDigitsVsNonDigits.py
@@ -0,0 +1,18 @@
+import re
+import sys
+import unittest
+
+
+def matchDigitVsNonDigits(test_string):
+    regex_pattern = r"(\d{2})\D(\d{2})\D(\d{4})$"
+    return re.match(regex_pattern, test_string ) is not None
+
+
+
+class MatchDigitVsNonDigits(unittest.TestCase):
+
+    def testCase_matchDigitVsNonDigits_GivenMultiSetOfInputd_ShouldReturnExpected(var):
+        var.assertEqual(matchDigitVsNonDigits("06-11-2015"), True, "Should return true")
+
+if __name__ == "__main__":
+    unittest.main()
