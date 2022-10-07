bdea3a1a184fb3246ef837eebdd0c786ca8bbc3a
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-10-07 20:20:29 +0200

    Day 22

---
+++ 30DaysOfCoding/Day22/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+class Node{
+    public Node left,right;
+    public int data;
+    public Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class Solution{
+
+	static int getHeight(Node root){
+        return Math.Max(
+            root.left != null ? 1 + getHeight( root.left) : 0,
+            root.right != null ?1 + getHeight(root.right ) : 0
+        );
+    }
+
+	static Node insert(Node root, int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    static void Main(String[] args){
+        Node root=null;
+        int T=Int32.Parse(Console.ReadLine());
+        while(T-->0){
+            int data=Int32.Parse(Console.ReadLine());
+            root=insert(root,data);            
+        }
+        int height=getHeight(root);
+        Console.WriteLine(height);
+        
+    }
+}
