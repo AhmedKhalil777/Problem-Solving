744b63d380b08da2a52b76f2f87c3734cb26e233
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-10-21 16:46:04 +0200

    Add Two Numbers - LeetCode #24

---
+++ Algorithms/LeetCode.Algorithms.Tests/A/AddTowNumbers.Tests.cs
@@ -0,0 +1,30 @@
+﻿using static LeetCode.Algorithms.AddTowNumbers;
+
+namespace LeetCode.Algorithms.Tests;
+
+using System.Text.Json;
+
+using ListNode = Algorithms.AddTowNumbers.ListNode;
+public class AddTowNumbers
+{
+    [Fact]
+    public void AddTwoNumbers_GivenMultiSet_ShouldReturnExpectedRes()
+    {
+        var l1 = new ListNode(2, new(4, new(3)));
+        var l2 = new ListNode(5, new(6, new(4)));
+        var expected = new ListNode(7, new(0, new(8)));
+        var res = JsonSerializer.Serialize(Algorithms.AddTowNumbers.AddTwoNumbers(l1, l2));
+        res.Should().Be(JsonSerializer.Serialize(expected));
+    }
+
+
+    [Fact]
+    public void AddTwoNumbers_GivenSet2_ShouldReturnExpectedRes()
+    {
+        var l1 = new ListNode(2, new(4, new(9)));
+        var l2 = new ListNode(5, new(6, new(4, new (9))));
+        var expected = new ListNode(7, new(0, new(4, new(0, new(1)))));
+        var res = JsonSerializer.Serialize(Algorithms.AddTowNumbers.AddTwoNumbers(l1, l2));
+        res.Should().Be(JsonSerializer.Serialize(expected));
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/A/AddTowNumbers.cs
@@ -0,0 +1,54 @@
+﻿using System.Numerics;
+using System.Text;
+
+namespace LeetCode.Algorithms;
+
+public class AddTowNumbers
+{
+    //Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val { get; set; }
+        public ListNode next { get; set; }
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+    public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        var res = new ListNode();
+        var stringBuilder = new StringBuilder();
+        while (l1 != null)
+        {
+            stringBuilder.Append(l1.val);
+            l1 = l1.next;
+        }
+        BigInteger firstNum = BigInteger.Parse(string.Join("",stringBuilder.ToString().Reverse()));
+        stringBuilder.Clear();
+        while (l2 != null)
+        {
+            stringBuilder.Append(l2.val);
+            l2 = l2.next;
+        }
+        BigInteger lastNum = BigInteger.Parse(string.Join("", stringBuilder.ToString().Reverse()));
+        var sum = (firstNum + lastNum).ToString().Reverse()
+            .ToList();  
+        var node = res;
+        while (true)
+        {
+            node.val = int.Parse(sum.First().ToString());
+            sum.RemoveAt(0);
+            if (sum.Count == 0)
+            {
+                break;
+            }
+            node.next = new ListNode();
+            node = node.next;
+        }
+        return res;
+    }
+}
