ea2bd2eac567d56bf0b05d68da4754bbae470067
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-10-21 09:35:52 +0200

    contains-duplicate-ii - Leet Code #22

---
+++ Algorithms/LeetCode.Algorithms.Tests/C/ContainsDuplicateII.Tests.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Algorithms.Tests;
+
+public class ContainsDuplicateII
+{
+    [Fact]
+    public void ContainsNearbyDuplicate_GivenMultiSet_ShouldReturnExpectedRes()
+    {
+        Algorithms.ContainsDuplicateII.ContainsNearbyDuplicate(new int[] { 1, 2, 3, 1 }, 3)
+            .Should().BeTrue();
+        Algorithms.ContainsDuplicateII.ContainsNearbyDuplicate(new int[] { 1, 0, 1, 1 }, 1)
+         .Should().BeTrue();
+        Algorithms.ContainsDuplicateII.ContainsNearbyDuplicate(new int[] { 1, 2, 3, 1, 2, 3 }, 2)
+          .Should().BeFalse();
+        Algorithms.ContainsDuplicateII.ContainsNearbyDuplicate(new int[] { 99,99 }, 2)
+            .Should().BeTrue();
+    }
+}
