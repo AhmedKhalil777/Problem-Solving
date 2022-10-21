d6b8678586a62249c1c4edbe7857669e0ffa1b26
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-10-21 18:41:03 +0200

    Longest Substring Without Repeating Characters - LeetCode #25

---
+++ Algorithms/LeetCode.Algorithms.Tests/L/LongestSubstring.Tests.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Algorithms.Tests;
+
+public class LongestSubstring
+{
+
+    [Fact]
+    public void LengthOfLongestSubstring_GivenMultiSet_ShouldReturnExpectedRes()
+    {
+        Algorithms.LongestSubstring.LengthOfLongestSubstring("abcabcbb")
+            .Should().Be(3);
+        Algorithms.LongestSubstring.LengthOfLongestSubstring("bbbbb")
+             .Should().Be(1);
+        Algorithms.LongestSubstring.LengthOfLongestSubstring("pwwkew")
+             .Should().Be(3);
+        Algorithms.LongestSubstring.LengthOfLongestSubstring("dvdf")
+            .Should().Be(3);
+    }   
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/L/LongestSubstring.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace LeetCode.Algorithms;
+
+public class LongestSubstring
+{
+    public static int LengthOfLongestSubstring(string s)
+    {
+            var maxSub = 0;
+            var n = 0;
+            var builder = new StringBuilder();
+            foreach (var c in s)
+            {
+                if (builder.ToString().Any(x=> x == c))
+                {
+                    var res = builder.ToString();
+                    var index = res.LastIndexOf(c)+1;
+                    res = res[index..];
+                    builder.Clear();
+                    builder.Append(res);
+                    n = 0;
+                }
+                builder.Append(c);
+                n = builder.Length;
+                if (n > maxSub) maxSub = n;
+            }
+            return maxSub;
+    }
+}
