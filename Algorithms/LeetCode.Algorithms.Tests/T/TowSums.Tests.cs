18a0f4b2d1ba529baad847b440b3012a217dcde6
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-10-21 09:12:35 +0200

    Leet code 2 Sums

---
+++ Algorithms/LeetCode.Algorithms.Tests/T/TowSums.Tests.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode.Algorithms.Tests;
+
+public class TowSums
+{
+    [Fact]
+    public void TowSums_GivenMultiSet_ShouldReturnExpectedRes()
+    {
+        Algorithms.TowSum.TwoSum(new int[] { 3, 2, 4 }, 6)
+            .Should().Equal(1,2);
+    }
+
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/T/TowSum.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Algorithms;
+
+public class TowSum
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        var res = new List<int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    res.Add(i);
+                    res.Add(j);
+                }
+            }
+        }
+        return res.ToArray();
+    }
+}
