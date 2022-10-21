41586284fcb4cff58b6f76af79364d39e470deab
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-10-21 09:49:01 +0200

    Contains Duplicate II & Contains Duplicate III - Leet Code #23

---
+++ Algorithms/LeetCode.Algorithms.Tests/C/ContainsDuplicate.Tests.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.Algorithms.Tests;
+
+public class ContainsDuplicate
+{
+
+
+    [Fact]
+    public void ContainsNearbyAlmostDuplicate_GivenMultiSet_ShouldReturnExpectedRes()
+    {
+        Algorithms.ContainsDuplicate.ContainsNearbyAlmostDuplicate(new int[] { 1, 2, 3, 1 }, 3, 0)
+            .Should().BeTrue();
+        Algorithms.ContainsDuplicate.ContainsNearbyAlmostDuplicate(new int[] { 1, 5, 9, 1, 5, 9 },2,3)
+         .Should().BeFalse();
+    }
+
+    [Fact]
+    public void ContainsNearbyDuplicate_GivenMultiSet_ShouldReturnExpectedRes()
+    {
+        Algorithms.ContainsDuplicate.ContainsNearbyDuplicate(new int[] { 1, 2, 3, 1 }, 3)
+            .Should().BeTrue();
+        Algorithms.ContainsDuplicate.ContainsNearbyDuplicate(new int[] { 1, 0, 1, 1 }, 1)
+         .Should().BeTrue();
+        Algorithms.ContainsDuplicate.ContainsNearbyDuplicate(new int[] { 1, 2, 3, 1, 2, 3 }, 2)
+          .Should().BeFalse();
+        Algorithms.ContainsDuplicate.ContainsNearbyDuplicate(new int[] { 99,99 }, 2)
+            .Should().BeTrue();
+    }
+
+    [Fact]
+    public void ContainsDuplicate_GivenMultiSet_ShouldReturnExpectedRes()
+    {
+        Algorithms.ContainsDuplicate.ContainsDuplicateI(new int[] { 1, 2, 3, 1 })
+            .Should().BeTrue();
+        Algorithms.ContainsDuplicate.ContainsDuplicateI(new int[] { 1, 0, 1, 1 })
+         .Should().BeTrue();
+        Algorithms.ContainsDuplicate.ContainsDuplicateI(new int[] { 1, 2, 3, 1, 2, 3 })
+          .Should().BeTrue();
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/C/ContainsDuplicate.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCode.Algorithms;
+
+public class ContainsDuplicate
+{
+
+    public static bool ContainsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length && j - i <= indexDiff; j++)
+            {
+                if (Math.Abs(nums[i] - nums[j]) <= valueDiff)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j  = i + 1; j  < nums.Length && j - i <= k; j ++)
+            {
+                if (nums[i] == nums[j] )
+                {
+                    return true;   
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public static bool ContainsDuplicateI(int[] nums)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j])
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
