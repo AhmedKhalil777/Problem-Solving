7d8033d83dd5970d93ded959a88a054ebeff8d53
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-11-17 21:06:57 +0200

    Reverse Integer LeetCode #28

---
+++ Algorithms/LeetCode.Algorithms.Tests/R/ReverseInteger.Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms.Tests;
+
+public class ReverseInteger
+{
+    [Fact]
+    public void Reverse_GivenMultiSet_ShouldReturnExpectedReverse()
+    {
+        Algorithms.ReverseInteger.Reverse(1534236469).
+            Should().Be(0);     
+        Algorithms.ReverseInteger.Reverse(-2147483648).
+            Should().Be(0);
+
+
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/R/ReverseInteger.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Algorithms;
+
+public static class ReverseInteger
+{
+    public static int Reverse(int x)
+    {
+        var isNegative = x < 0;
+        try
+        {
+            var r = string.Join(string.Empty, Math.Abs(x).ToString().Reverse());
+            if (isNegative) r = "-" + r;
+            return int.Parse(r);
+
+        }
+        catch (OverflowException)
+        {
+            return 0;
+        }
+
+    }
+
+}
