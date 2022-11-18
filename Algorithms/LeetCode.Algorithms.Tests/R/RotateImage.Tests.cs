33278f15ea6774a082863587166e3bee49b692d5
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-11-18 21:12:37 +0200

    Rotate Image Leet Code #35

---
+++ Algorithms/LeetCode.Algorithms.Tests/R/RotateImage.Tests.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode.Algorithms.Tests;
+
+public class RotateImage
+{
+    [Fact]
+    public void RotateTests()
+    {
+        Algorithms.RotateImage.Rotate(new int[3][] { new int[3] { 1, 2, 3 }, new int[3] { 4, 5, 6 }, new int[3] { 7, 8, 9 } })
+            .Should().BeEquivalentTo(new int[3][] { new int[3] { 7, 4, 1 }, new int[3] { 8, 5, 2 }, new int[3] { 9, 6, 3 } });
+
+    }
+
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/R/RotateImage.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Algorithms;
+
+public static class RotateImage
+{
+    public static int[][] Rotate(int[][] matrix)
+    {
+        var cMatrix = matrix.Select(x=>x.ToList()).ToList();
+        for (int i = 0 ; i < matrix.Length; i++)
+        { 
+            for (int j = 0; j < matrix.Length  ; j++)
+            {
+                matrix[i][j] = cMatrix[j][i];
+            }
+        }
+        cMatrix = matrix.Select(x => x.ToList()).ToList();
+        for (int i = 0, m= matrix.Length -1; i < matrix.Length; i++, m--)
+        {
+            for (int j = 0; j < matrix.Length; j++)
+            {
+                matrix[j][i] = cMatrix[j][m];
+            }
+        }
+        return matrix;
+    }
+}
