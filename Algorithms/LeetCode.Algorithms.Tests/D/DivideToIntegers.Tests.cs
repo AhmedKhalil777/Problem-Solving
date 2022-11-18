8cd4450e9690653811c4a7f3912af72e5b454eee
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-11-18 11:48:43 +0200

    Divide To Integers LeetCode #32

---
+++ Algorithms/LeetCode.Algorithms.Tests/D/DivideToIntegers.Tests.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode.Algorithms.Tests;
+
+public class DivideToIntegers
+{
+
+    [Fact]
+    public void DivideTest()
+    {
+        Algorithms.DivideToIntegers.Divide(-2147483648, -1)
+            .Should().Be(int.MaxValue);
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/D/DivideToIntegers.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Algorithms;
+
+public class DivideToIntegers
+{
+    public static int Divide(int dividend, int divisor)
+    {
+        try
+        {
+            return dividend / divisor;
+
+        }
+        catch (OverflowException)
+        {
+            if (dividend > 0 && divisor > 0 || dividend < 0 && divisor < 0)
+            {
+                return int.MaxValue;
+            }
+            return int.MinValue;  
+        }
+        catch (Exception)
+        {
+            return 0;
+        }
+    }
+}
