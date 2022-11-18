2a681df21ae05b8c7bfb1f5cebc2c06d6478bdef
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-11-18 16:42:33 +0200

    Ugly Number Leet Code #34

---
+++ Algorithms/LeetCode.Algorithms.Tests/U/UglyNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms.Tests
+{
+    public class UglyNumber
+    {
+        [Fact]
+        public void IsUglyTests()
+        {
+            Algorithms.UglyNumber.IsUgly(1).Should().BeTrue();
+            Algorithms.UglyNumber.IsUgly(6).Should().BeTrue();
+            Algorithms.UglyNumber.IsUgly(14).Should().BeFalse();
+        }
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/U/UglyNumber.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Algorithms;
+
+public class UglyNumber
+{
+    public static bool IsUgly(int n)
+    {
+        if (n == 0)
+            return true;
+
+        while(n % 2  == 0)
+        {
+            n /= 2;
+        }
+        while (n % 3 == 0)
+        {
+            n /= 3;
+        }
+        while (n % 5 == 0)
+        {
+            n /= 5;
+        }
+        if (n == 1 )
+            return true;
+        return false;
+    }
+}
