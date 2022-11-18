877b2763971191282188e45fe07d71b021e83e59
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-11-18 11:58:39 +0200

    Multiply Strings Leet Code #33

---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/M/MultiplyStrings.cs
@@ -0,0 +1,14 @@
+﻿using static IronPython.Modules._ast;
+using System.Numerics;
+
+namespace LeetCode.Algorithms;
+
+public class MultiplyStrings
+{
+    public string Multiply(string num1, string num2)
+    {
+        var x = BigInteger.Parse(num1);
+        var y = BigInteger.Parse(num2);
+        return (x * y).ToString();
+    }
+}
