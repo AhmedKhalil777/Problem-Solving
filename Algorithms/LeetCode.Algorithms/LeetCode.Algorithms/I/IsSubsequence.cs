9684ed808fb873caebc4b3a2416879e94e0a4624
AhmedKhalilIS <ahmed.khalil@iskraemeco.com>
2022-12-23 21:53:58 +0200

    Is Subsequence Leet Code #42

---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/I/IsSubsequence.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Algorithms;
+
+public static class IsSubsequenceC
+{
+    public static bool IsSubsequence(string s, string t)
+    {
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (!t.Contains(s[i]))
+            {
+                return false;
+            }
+            t = t[(t.IndexOf(s[i])+1)..];
+        }
+
+        return true;
+    }
+}
