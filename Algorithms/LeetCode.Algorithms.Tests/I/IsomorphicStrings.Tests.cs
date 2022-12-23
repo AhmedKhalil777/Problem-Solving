25f3d62449ed56e90621bf02c156eddcdc62209c
AhmedKhalilIS <ahmed.khalil@iskraemeco.com>
2022-12-23 21:33:43 +0200

    Isomorphic Strings Leet Code #41

---
+++ Algorithms/LeetCode.Algorithms.Tests/I/IsomorphicStrings.Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms.Tests;
+
+public class IsomorphicStrings
+{
+    [Fact]
+    public void IsIsomorphic()
+    {
+        Algorithms.IsomorphicStrings.IsIsomorphic(s: "egg", t: "add").Should().BeTrue();
+        Algorithms.IsomorphicStrings.IsIsomorphic(s: "foo", t: "bar").Should().BeFalse();
+        Algorithms.IsomorphicStrings.IsIsomorphic(s: "paper", t: "title").Should().BeTrue();
+        Algorithms.IsomorphicStrings.IsIsomorphic(s: "badc", t: "baba").Should().BeFalse();
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/I/IsomorphicStrings.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.Algorithms;
+
+public static class IsomorphicStrings
+{
+
+    public static bool IsIsomorphic(string s, string t)
+    {
+        if (s.Length != t.Length)
+        {
+            return false;
+        }
+        var dict = new Dictionary<char, char>();
+        var rdict = new Dictionary<char, char>();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (dict.TryGetValue(s[i], out char c))
+            {
+                if (t[i] != c)
+                {
+                    return false;
+                }
+                continue;
+            }
+
+            if (rdict.TryGetValue(t[i], out char cc))
+            {
+                if (s[i] != cc)
+                {
+                    return false;
+                }
+                continue;
+            }
+            dict.TryAdd(s[i], t[i]);
+            rdict.TryAdd(t[i], s[i]);
+        }
+
+        return true;
+
+    }
+}
