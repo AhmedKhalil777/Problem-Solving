636c4b288b3a82a9b33af9533a1fa5c9062a4ecb
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-12-25 21:39:25 +0200

    35. Search Insert Position Leet Code #52

---
+++ Algorithms/LeetCode.Algorithms.Tests/S/SearchInsertPosition.Tests.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Algorithms.Tests;
+
+public class SearchInsertPosition
+{
+    [Fact]
+    public void SearchInsert()
+    {
+        Algorithms.SearchInsertPosition.SearchInsert(new int[] { 1, 3, 5, 6 }, 5)
+            .Should().Be(2);
+        Algorithms.SearchInsertPosition.SearchInsert(new int[] { 1, 3, 5, 6 }, 2)
+         .Should().Be(1);
+        Algorithms.SearchInsertPosition.SearchInsert(new int[] { 1, 3, 5, 6 }, 7)
+          .Should().Be(4);
+        Algorithms.SearchInsertPosition.SearchInsert(new int[] { 1, 3, 5, 6 }, 0)
+            .Should().Be(0);
+        Algorithms.SearchInsertPosition.SearchInsert(new int[] { 1 }, 1)
+         .Should().Be(0);
+    }
+}
+    
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/S/SearchInsertPosition.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms;
+
+public static class SearchInsertPosition
+{
+    public static int SearchInsert(int[] nums, int target)
+    {
+        var l = nums.ToList();
+        l.Add(int.MaxValue);
+        nums = l.ToArray();
+
+        int minNum = 0;
+        int maxNum = nums.Length - 1;
+        int mid = 0;
+
+        while (minNum <= maxNum)
+        {
+            mid = (minNum + maxNum) / 2;
+            if (target >= nums[mid] && target < nums[mid+1])
+            {
+                if (target == nums[mid])
+                {
+                    return mid;
+                }
+                return mid +1;
+            }
+            else if (target < nums[mid])
+            {
+                maxNum = mid - 1;
+            }
+            else
+            {
+                minNum = mid + 1;
+            }
+        }
+
+        return mid ;
+    }
+}
