cdd1345dad75aa0d56e0203b388e031d83b53c49
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-12-25 16:11:36 +0200

    6. Zigzag Conversion Leet Code #49

---
+++ Algorithms/LeetCode.Algorithms.Tests/Z/ZigzagConversion.Tests.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode.Algorithms.Tests;
+
+public class ZigzagConversion
+{
+    [Fact]
+    public void Convert()
+    {
+        Algorithms.ZigzagConversion.Convert("PAYPALISHIRING", 3).Should().Be("PAHNAPLSIIGYIR");
+        Algorithms.ZigzagConversion.Convert("PAYPALISHIRING", 4).Should().Be("PINALSIGYAHRPI");
+        Algorithms.ZigzagConversion.Convert("A", 1).Should().Be("A");
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/Z/ZigzagConversion.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace LeetCode.Algorithms;
+
+public static class ZigzagConversion
+{
+    public static string Convert(string s, int numRows)
+    {
+        if (numRows ==1)
+        {
+            return s;
+        }
+
+        var ordering = new List<Tuple<int, char>>();
+        var i = 0;
+        var dir = 1;
+        foreach (var c in s)
+        {
+            ordering.Add(new Tuple<int, char>(i, c));
+            
+            if (i == numRows -1)
+            {
+                dir = -1;
+            }
+            else if (i == 0)
+            {
+                dir = 1;
+            }
+            
+            i = dir == 1 ? i + 1 : i-1;
+        }
+        var sb = new StringBuilder();
+        for (int j = 0; j < numRows; j++)
+        {
+            sb.Append(string.Join(string.Empty,ordering.Where(x => x.Item1 == j).Select(x => x.Item2)));
+        }
+
+        return sb.ToString();
+
+    }
+}
