aa46421e6edfb9d936632fee8fedae12e86f1cef
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-12-25 20:49:20 +0200

    278. First Bad Version Leet Code #51

---
+++ Algorithms/LeetCode.Algorithms.Tests/F/FirstBadVersion.Tests.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace LeetCode.Algorithms.Tests;
+
+public class VersionControl
+{
+
+    [Fact]
+    public void FirstBadVersion()
+    {
+        var versionC = new Algorithms.VersionControl();
+        versionC.SetBadVersion(4);
+        versionC.FirstBadVersion(5).Should().Be(4);
+        versionC.SetBadVersion(1);
+        versionC.FirstBadVersion(1).Should().Be(1);
+        versionC.SetBadVersion(2);
+        versionC.FirstBadVersion(2).Should().Be(2);  
+        versionC.SetBadVersion(1702766719);
+        versionC.FirstBadVersion(2126753390).Should().Be(1702766719);
+
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/F/FirstBadVersion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static IronPython.Modules._ast;
+
+namespace LeetCode.Algorithms;
+
+public class VersionControl : VersionControlBase
+{
+    public int FirstBadVersion(int n)
+    {
+        long minNum = 0;
+        long maxNum = n ;
+        while (minNum <= maxNum)
+        {
+            long mid =  (minNum + maxNum) / 2;
+            if (IsBadVersion((int)mid) && !IsBadVersion((int)mid -1))
+            {
+                return (int)mid;
+            }
+            else if (IsBadVersion((int)mid))
+            {
+                maxNum = mid - 1;
+            }
+            else
+            {
+                minNum = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
+
+
+public abstract class VersionControlBase
+{
+    public void SetBadVersion(int badVersion)
+    {
+        FirstBadVersion= badVersion;
+    }
+    private int FirstBadVersion { set; get; }
+    protected bool IsBadVersion(int version)
+    {
+        return version>= FirstBadVersion;
+    }
+}
