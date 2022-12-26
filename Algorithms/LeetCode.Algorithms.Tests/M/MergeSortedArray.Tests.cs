9392e5a1ac421190caf4f172bb2d6d46fa8a4f6f
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-12-26 10:03:31 +0200

    88. Merge Sorted Array Leet Code #57

---
+++ Algorithms/LeetCode.Algorithms.Tests/M/MergeSortedArray.Tests.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Algorithms.Tests;
+
+public class MergeSortedArray
+{
+    [Fact]
+    public void Merge()
+    {
+        Algorithms.MergeSortedArray.Merge(new int[] {1, 2, 3, 0, 0, 0 }, 3,new int[] { 2, 5, 6 },3)
+            .Should().Equal(new int[] { 1, 2, 2, 3, 5, 6 });
+        Algorithms.MergeSortedArray.Merge(new int[] { 1}, 1, new int[] { }, 0)
+             .Should().Equal(new int[] { 1 });
+        Algorithms.MergeSortedArray.Merge(new int[] { 0 }, 0, new int[] {1 }, 1)
+             .Should().Equal(new int[] { 1 });
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/M/MergeSortedArray.cs
@@ -0,0 +1,52 @@
+﻿
+namespace LeetCode.Algorithms;
+
+public static class MergeSortedArray
+{
+    public static int[] Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        var j = 0; 
+        var k = 0;
+        var nums3 = new int[nums1.Length];
+        nums1.CopyTo(nums3, 0);
+        for (int i = 0; i < n+m; i++)
+        {
+
+            if (j >= m  )
+            {
+                nums1[i] = nums2[k];
+                k++;
+                continue;
+            }
+
+            if (k >= n  )
+            {
+                nums1[i] = nums3[j];
+                j++;
+                continue;
+            }
+
+            if (nums3[j] > nums2[k])
+            {
+                nums1[i] = nums2[k];
+                k++;
+                continue;
+            }
+
+            if (nums3[j] < nums2[k])
+            {
+                nums1[i] = nums3[j];
+                j++;
+                continue;
+            }
+
+            if (nums3[j] == nums2[k])
+            {
+                nums1[i++] = nums3[j++];
+                nums1[i] = nums2[k++];
+
+            }
+        }
+        return nums1;
+    }
+}
