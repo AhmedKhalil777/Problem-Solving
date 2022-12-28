1644065e88adad3b107a2314c0f5a210be33a8d2
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-12-28 15:15:49 +0200

    309. Best Time to Buy and Sell Stock with Cooldown Leet Code #45

---
+++ Algorithms/LeetCode.Algorithms.Tests/B/BestTimetoBuyandSellStockwithCooldown.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms.Tests;
+
+public class BestTimetoBuyandSellStockwithCooldown
+{
+    [Fact]
+    public void MaxProfit()
+    {
+        Algorithms.BestTimetoBuyandSellStockwithCooldown.MaxProfit(new int[] { 7, 1, 5, 3, 6, 4 })
+            .Should().Be(5);   
+        Algorithms.BestTimetoBuyandSellStockwithCooldown.MaxProfit(new int[] { 7, 6, 4, 3, 1 })
+            .Should().Be(0);
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/B/BestTimetoBuyandSellStockwithCooldown.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Algorithms;
+
+public static class BestTimetoBuyandSellStockwithCooldown
+{
+    public static int MaxProfit(int[] prices)
+    {
+        var minimum = prices[0];
+        var maxProfit = 0;
+        for (int i = 0; i < prices.Length; i++)
+        {
+            minimum = Math.Min(minimum, prices[i]);
+            maxProfit = Math.Max(maxProfit, prices[i] - minimum);
+        }
+        return maxProfit;
+    }
+}
