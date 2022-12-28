00e54addf4467f49d2234de66729ec19a1c1a6be
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-12-28 18:16:00 +0200

    283. Move Zeroes Leet Code #66

---
+++ Algorithms/LeetCode.Algorithms.Tests/M/Move Zeroes.Tests.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Algorithms.Tests;
+
+public class Move_Zeroes
+{
+    [Fact]
+    public void MoveZeroes()
+    {
+        var actual1 = new int[] { 0, 1, 0, 3, 12 };
+        var expected1 = new int[] { 1, 3, 12, 0, 0 };
+        Algorithms.Move_Zeroes.MoveZeroes(actual1);
+        actual1.Should().Equal(expected1);
+        //////////////////////
+        var actual2 = new int[] { 0 };
+        var expected2 = new int[] { 0 };
+        Algorithms.Move_Zeroes.MoveZeroes(actual2);
+        actual2.Should().Equal(expected2);
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/M/Move Zeroes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms;
+
+public static class Move_Zeroes
+{
+    public static void MoveZeroes(int[] nums)
+    {
+        int j = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[j++] = nums[i];
+            }
+        }
+        for (; j < nums.Length; j++)
+        {
+            nums[j] = 0;
+        }
+    }
+
+
+    public static void MoveZeroes1(int[] nums)
+    {
+        var zeroezCount = nums.Count(x => x == 0);
+        var res = nums.ToList();
+        res.RemoveAll(x => x == 0);
+        res.AddRange(Enumerable.Repeat(0, zeroezCount));
+        res.ToArray().CopyTo(nums, 0);
+    }
+}
