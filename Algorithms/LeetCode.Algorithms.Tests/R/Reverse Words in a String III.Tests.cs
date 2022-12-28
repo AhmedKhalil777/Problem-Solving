2c50536fe58e462cee7f86047b691795d5743d40
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-12-28 19:48:51 +0200

    557. Reverse Words in a String III Leet Code #68

---
+++ Algorithms/LeetCode.Algorithms.Tests/R/Reverse Words in a String III.Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms.Tests;
+
+public class Reverse_Words_in_a_String_III
+{
+    [Fact]
+    public void ReverseWords()
+    {
+        Algorithms.Reverse_Words_in_a_String_III.ReverseWords("Let's take LeetCode contest")
+            .Should().Be("s'teL ekat edoCteeL tsetnoc");
+
+        Algorithms.Reverse_Words_in_a_String_III.ReverseWords("God Ding")
+            .Should().Be("doG gniD");
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/R/Reverse Words in a String III.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms;
+
+public static class Reverse_Words_in_a_String_III
+{
+    public static string ReverseWords(string s)
+    {
+        return string.Join(" ", s.Split(' ').Select(x => string.Concat(x.Reverse())));
+    }
+}
