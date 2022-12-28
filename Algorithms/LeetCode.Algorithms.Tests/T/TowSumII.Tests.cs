2f7aeeced872b0be9239be20280e0d77c8c74d78
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-12-28 18:50:36 +0200

    167. Two Sum II - Input Array Is Sorted Leet Code #67

---
+++ Algorithms/LeetCode.Algorithms.Tests/T/TowSumII.Tests.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Algorithms.Tests;
+
+public class TowSumII
+{
+    [Fact]
+    public void TowSum()
+    {
+        Algorithms.Two_Sum_II.TwoSum(new int[] { 2, 7, 11, 15 }, 9)
+            .Should().Equal(new int[] { 1, 2 });
+        Algorithms.Two_Sum_II.TwoSum(new int[] { 2, 3, 4 }, 6)
+            .Should().Equal(new int[] { 1, 3 });
+        Algorithms.Two_Sum_II.TwoSum(new int[] { -1, 0 }, -1)
+            .Should().Equal(new int[] { 1, 2 });
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/T/Two Sum II.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace LeetCode.Algorithms;
+
+public static class Two_Sum_II
+{
+    /// <summary>
+    /// The numbers are sorted so use the binary search is the valuable algorithm
+    /// </summary>
+    /// <param name="numbers"></param>
+    /// <param name="target"></param>
+    /// <returns></returns>
+    public static int[] TwoSum(int[] numbers, int target)
+    {
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            var j = BinarySearch.Search(numbers, target - numbers[i]);
+            if (j!= -1)
+            {
+                if (i == j)
+                {
+                    ++j;
+                    return new int[] { ++i, ++j };
+                }
+                return new int[] { ++i, ++j };
+            }
+        }
+        return null;   
+
+    }
+
+
+}
