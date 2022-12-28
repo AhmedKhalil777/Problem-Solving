a46ed80e5bcfce0c479929d856d10948456b1c45
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-12-28 20:05:36 +0200

    350. Intersection of Two Arrays II Leet Code #69

---
+++ Algorithms/LeetCode.Algorithms.Tests/I/Intersection of Two Arrays II.Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms.Tests;
+
+public class Intersection_of_Two_Arrays_II
+{
+    [Fact]
+    public void Instersect()
+    {
+        Algorithms.Intersection_of_Two_Arrays_II.Intersect(new int[] { 1, 2, 2, 1 }, new int[] { 2, 2 })
+            .Should().Equal(new int[] { 2, 2 });
+
+        Algorithms.Intersection_of_Two_Arrays_II.Intersect(new int[] { 4, 9, 5 }, new int[] { 9, 4, 9, 8, 4 })
+             .Should().Equal(new int[] { 4, 9 });
+
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/I/Intersection of Two Arrays II.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms;
+
+public static class Intersection_of_Two_Arrays_II
+{
+    public static int[] Intersect(int[] nums1, int[] nums2)
+    {
+        var longest = nums1.Length >= nums2.Length ? nums1.ToList() : nums2.ToList();
+        var shortest = nums1.Length < nums2.Length ? nums1.ToList() : nums2.ToList();
+        var res = new List<int>();
+        for (int i = 0; i < shortest.Count; i++)
+        {
+            if (longest.Any(x=> x == shortest[i]))
+            {
+                res.Add(shortest[i]);
+                longest.Remove(shortest[i]);
+            }
+        }
+        return res.ToArray();
+    }
+}
