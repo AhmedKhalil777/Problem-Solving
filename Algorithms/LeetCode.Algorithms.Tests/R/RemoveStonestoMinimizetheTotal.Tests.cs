2a55288b7f3e6c8c19b936ed6024a40f1107e48e
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-12-28 14:42:40 +0200

    1962. Remove Stones to Minimize the Total Leet Code #62

---
+++ Algorithms/LeetCode.Algorithms.Tests/R/RemoveStonestoMinimizetheTotal.Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms.Tests;
+
+public class RemoveStonestoMinimizetheTotal
+{
+    [Fact]
+    public void MinStoneSum()
+    {
+        Algorithms.RemoveStonestoMinimizetheTotal.MinStoneSum(new int[] { 5, 4, 9 }, 2).Should().Be(12);
+        Algorithms.RemoveStonestoMinimizetheTotal.MinStoneSum(new int[] { 4, 3, 6, 7 }, 3).Should().Be(12);
+        Algorithms.RemoveStonestoMinimizetheTotal.MinStoneSum(new int[] { 4122, 9928, 3477, 9942 }, 6).Should().Be(8768);
+    }
+
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/R/RemoveStonestoMinimizetheTotal.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Algorithms;
+
+public static class RemoveStonestoMinimizetheTotal
+{
+    public static int MinStoneSum(int[] piles, int k)
+    {
+        var queue = new PriorityQueue<int, int>();
+        for (int i = 0; i < piles.Length; i++)
+        {
+            queue.Enqueue(piles[i], -piles[i]);
+        }
+
+        for (int i = 0; i < k; i++)
+        {
+            var s = queue.Dequeue();
+            s -=(int) Math.Floor((decimal)s / 2);
+            queue.Enqueue(s, -s);
+        }
+        var sum = 0;
+        while(queue.TryDequeue(out int value, out int pri))
+        {
+            sum += value;
+        }
+        return sum;
+    }
+}
