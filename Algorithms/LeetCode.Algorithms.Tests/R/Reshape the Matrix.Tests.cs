672ff993169fc4a8f5e05028960017d3f656e525
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-12-28 23:33:24 +0200

    566. Reshape the Matrix Leet Code #70

---
+++ Algorithms/LeetCode.Algorithms.Tests/R/Reshape the Matrix.Tests.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Algorithms.Tests;
+
+[UsesVerify]
+public class Reshape_the_Matrix
+{
+    [Fact]
+    public async Task MatrixReshape()
+    {
+        var mat = new int[2][];
+        mat[0] = new int[] { 1, 2 }; 
+        mat[1] = new int[] { 3, 4 };
+        await Verify(Algorithms.Reshape_the_Matrix.MatrixReshape(mat, 1, 4));
+    }
+
+    [Fact]
+    public async Task MatrixReshape2()
+    {
+        var mat = new int[2][];
+        mat[0] = new int[] { 1, 2 };
+        mat[1] = new int[] { 3, 4 };
+        await Verify(Algorithms.Reshape_the_Matrix.MatrixReshape(mat, 2, 4));
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/R/Reshape the Matrix.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Algorithms;
+
+public static class Reshape_the_Matrix
+{
+    public static int[][] MatrixReshape(int[][] mat, int r, int c)
+    {
+        var xLen = mat.Length;
+        var yLen = mat[0].Length;
+        if (xLen * yLen != r * c)
+        {
+            return mat;
+        }
+        var res = new int[r][];
+        for (int i = 0; i < r; i++)
+        {
+            res[i] = new int[c];
+        }
+
+        int m = 0, n = 0;
+        for (int i = 0; i < xLen; i++)
+        {
+            for (int j = 0; j < yLen; j++)
+            {
+                res[m][n] = mat[i][j];
+                n++;
+                if (n == c)
+                {
+                    n = 0;
+                    r++;
+                }
+            }
+        }
+        return res;
+
+    }
+}
