37877eab36c0b555a9947a91866cab01398e23ee
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-12-28 23:27:11 +0200

    Equalize the Array Hacker rank #71

---
+++ Algorithms/HackerRank.Algorithms/E/Equalize the Array.cs
@@ -0,0 +1,19 @@
+﻿namespace HackerRank.Algorithms;
+
+public static class Equalize_the_Array
+{
+    /*
+ * Complete the 'equalizeArray' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+    public static int equalizeArray(List<int> arr)
+    {
+        var arrLen = arr.Count;
+        var dict =arr.GroupBy(x=> x);
+        var max = dict.Max(x => x.Count());
+        return arrLen - max;
+    }
+}
