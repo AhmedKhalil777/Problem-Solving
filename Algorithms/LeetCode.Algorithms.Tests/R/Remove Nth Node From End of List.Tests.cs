e7e51fe5a63d0c0c51a5481216b8cc409e2386fe
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-12-29 20:58:19 +0200

    19. Remove Nth Node From End of List Leet Code #74

---
+++ Algorithms/LeetCode.Algorithms.Tests/R/Remove Nth Node From End of List.Tests.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.Algorithms.Tests;
+
+[UsesVerify]
+public class Remove_Nth_Node_From_End_of_List
+{
+    [Fact]
+    public async Task RemoveNthFromEnd()
+    {
+        var node = new ListNode(new int[] { 1, 2, 3, 4, 5});
+        await Verify(Algorithms.Remove_Nth_Node_From_End_of_List.RemoveNthFromEnd(node, 2));
+    }
+
+    [Fact]
+    public  void RemoveNthFromEnd_Nulls()
+    {
+        var node = new ListNode(new int[] { 1});
+        Algorithms.Remove_Nth_Node_From_End_of_List.RemoveNthFromEnd(node, 1).Should().BeNull();
+    }
+
+
+    [Fact]
+    public async Task RemoveNthFromEndI()
+    {
+        var node2 = new ListNode(new int[] { 1, 2 });
+        await Verify(Algorithms.Remove_Nth_Node_From_End_of_List.RemoveNthFromEnd(node2, 2));
+    }
+
+
+    [Fact]
+    public async Task RemoveNthFromEndII()
+    {
+        var node = new ListNode(new int[] { 1,2 });
+        await Verify(Algorithms.Remove_Nth_Node_From_End_of_List.RemoveNthFromEnd(node, 1));
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/R/Remove Nth Node From End of List.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Algorithms;
+
+public static class Remove_Nth_Node_From_End_of_List
+{
+    public static ListNode RemoveNthFromEnd(ListNode head, int n)
+    {
+        if (head == null)
+        {
+            return null;   
+        }
+        var stack = new Stack<ListNode>();
+        var index = head;
+        while (index != null)
+        {
+            stack.Push(index);
+            index = index.next;
+        }
+        ListNode tempNode  = null;
+        for (int i = 0; i < n; i++)
+        {
+           tempNode = stack.Pop();
+        }
+
+        var isPoped = stack.TryPop(out var indexBeforeN);
+        if (!isPoped) { return tempNode.next; }
+        indexBeforeN.next = indexBeforeN.next.next;
+        return head;
+    }
+}
