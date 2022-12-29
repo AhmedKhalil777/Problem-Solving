23ce181048b09fdc00b970be5184e999ee233697
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-12-29 22:36:41 +0200

    74. Search a 2D Matrix Leet Code #77

---
+++ Algorithms/LeetCode.Algorithms.Tests/S/Search a 2D Matrix.Tests.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace LeetCode.Algorithms.Tests;
+
+public class Search_a_2D_Matrix
+{
+
+    [Fact]
+    public void SearchMatrix()
+    {
+        var json = "[[1,3,5,7],[10,11,16,20],[23,30,34,60]]";
+        var actual = JsonConvert.DeserializeObject<int[][]>(json);
+        Algorithms.Search_a_2D_Matrix.SearchMatrix(actual,3).Should().BeTrue();
+        //////
+        json = "[[1,3,5,7],[10,11,16,20],[23,30,34,60]]";
+        actual = JsonConvert.DeserializeObject<int[][]>(json);
+        Algorithms.Search_a_2D_Matrix.SearchMatrix(actual, 13).Should().BeFalse();
+
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/S/Search a 2D Matrix.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Algorithms;
+
+public static class Search_a_2D_Matrix
+{
+    public static bool SearchMatrix(int[][] matrix, int target)
+    {
+        var flatten = matrix.SelectMany(x=> x).ToArray();
+        if (BinarySearch.Search(flatten, target) >= 0)
+        {
+            return true;
+        }
+        return false;
+    }
+}
