1e2f6b5dae79fa22bf9b331e09c8d516c78421b8
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-12-29 22:02:03 +0200

    36. Valid Sudoku Leet Code #76

---
+++ Algorithms/LeetCode.Algorithms.Tests/V/Valid Sudoku.Tests.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms.Tests;
+
+public class Valid_Sudoku
+{
+    [Fact]
+    public void IsValidSudoku()
+    {
+        var json = "[['5','3','.','.','7','.','.','.','.'],['6','.','.','1','9','5','.','.','.'],['.','9','8','.','.','.','.','6','.'],['8','.','.','.','6','.','.','.','3'],['4','.','.','8','.','3','.','.','1'],['7','.','.','.','2','.','.','.','6'],['.','6','.','.','.','.','2','8','.'],['.','.','.','4','1','9','.','.','5'],['.','.','.','.','8','.','.','7','9']]";
+        var actual = JsonConvert.DeserializeObject<char[][]>(json);
+        Algorithms.Valid_Sudoku.IsValidSudoku(actual).Should().BeTrue();
+
+    }
+
+    [Fact]
+    public void IsValidSudoku2()
+    {
+        var json = "[['8','3','.','.','7','.','.','.','.'],['6','.','.','1','9','5','.','.','.'],['.','9','8','.','.','.','.','6','.'],['8','.','.','.','6','.','.','.','3'],['4','.','.','8','.','3','.','.','1'],['7','.','.','.','2','.','.','.','6'],['.','6','.','.','.','.','2','8','.'],['.','.','.','4','1','9','.','.','5'],['.','.','.','.','8','.','.','7','9']]";
+        var actual = JsonConvert.DeserializeObject<char[][]>(json);
+        Algorithms.Valid_Sudoku.IsValidSudoku(actual).Should().BeFalse();
+
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/V/Valid Sudoku.cs
@@ -0,0 +1,87 @@
+﻿namespace LeetCode.Algorithms;
+
+public static class Valid_Sudoku
+{
+    public static bool IsValidSudoku(char[][] board)
+    {
+        const int cells = 9;
+        //Rows
+        for (int i = 0; i < cells; i++)
+        {
+            if (ContainsDuplicate(board[i].Where(x=> x!= '.').ToArray()))
+            {
+                return false;
+            }
+        }
+
+        // Columns
+        for (int i = 0; i < cells; i++)
+        {
+            var col = new List<char>();
+            for (int j = 0; j < cells; j++)
+            {
+                if (board[j][i] != '.')
+                {
+                    col.Add(board[j][i]);
+                }
+            }
+            if (ContainsDuplicate(col.ToArray()))
+            {
+                return false;
+            }
+        }
+
+
+        // Boxes
+        var s = 0;
+        var p = 0;
+        for (int i = 0; i < cells; i++)
+        {
+            var box = new char[9];
+            var m = 0;
+
+            if (s == 9)
+            {
+                s = 0;
+            }
+            if (p == 9)
+            {
+                p = 0;
+            }
+
+            for (int j = s; j < (s + 3); j++)
+            {
+                for (int k = p ; k < (p  + 3); k++)
+                {
+                    box[m++] = board[j][k];
+                }
+            }
+            p += 3;
+            if (p == 9)
+            {
+                s+=3;
+            }
+            if (ContainsDuplicate(box.Where(x=> x!= '.').ToArray()))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static bool ContainsDuplicate(char[] chars)
+    {
+        for (int i = 0; i < chars.Length; i++)
+        {
+            for (int j = i + 1; j < chars.Length; j++)
+            {
+                if (chars[i] == chars[j])
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
