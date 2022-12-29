14a0d75c01eb6f9211bfbadc76fdadbcaf8d34ab
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-12-29 20:21:58 +0200

    1834. Single-Threaded CPU Leet Code #73

---
+++ Algorithms/LeetCode.Algorithms.Tests/S/Single-Threaded CPU.Tests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms.Tests;
+
+public class Single_Threaded_CPU
+{
+    [Fact]
+    public void GetOrder()
+    {
+        var actual = new int[4][];
+        actual[0] = new int[] {1,2 };
+        actual[1] = new int[] {2,4 };
+        actual[2] = new int[] {3,2 };
+        actual[3] = new int[] {4,1 };
+        Algorithms.Single_Threaded_CPU.GetOrder(actual)
+            .Should().Equal(new int[] { 0, 2, 3, 1 });
+    }
+
+    [Fact]
+    public void GetOrder1()
+    {
+        var actual = new int[5][];
+        actual[0] = new int[] { 7, 10 };
+        actual[1] = new int[] { 7, 12 };
+        actual[2] = new int[] { 7, 5 };
+        actual[3] = new int[] { 7, 4 };
+        actual[4] = new int[] { 7, 2 };
+        Algorithms.Single_Threaded_CPU.GetOrder(actual)
+            .Should().Equal(new int[] {4,3,2, 0,1 });
+    }
+
+    [Fact]
+    public void GetOrder2()
+    {
+        var json = "[[19,13],[16,9],[21,10],[32,25],[37,4],[49,24],[2,15],[38,41],[37,34],[33,6],[45,4],[18,18],[46,39],[12,24]]";
+        var actual = JsonSerializer.Deserialize<int[][]>(json);
+        Algorithms.Single_Threaded_CPU.GetOrder(actual)
+            .Should().Equal(new int[] { 6, 1, 2, 9, 4, 10, 0, 11, 5, 13, 3, 8, 12, 7 });
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/S/Single-Threaded CPU.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Concurrent;
+
+using static Community.CsharpSqlite.Sqlite3;
+
+namespace LeetCode.Algorithms;
+
+public static class Single_Threaded_CPU
+{
+
+    public static int[] GetOrder(int[][] tasks)
+    {
+
+        Comparer<Task> enquethenProcessingTimeThenIndex = Comparer<Task>
+        .Create((t1, t2) => {
+            if (t1.EnqueueTime != t2.EnqueueTime)
+            {
+                return t1.EnqueueTime.CompareTo(t2.EnqueueTime);
+            }
+            if (t1.ProcessingTime != t2.ProcessingTime)
+            {
+                return t1.ProcessingTime.CompareTo(t2.ProcessingTime);
+            }
+            return t1.Index.CompareTo(t2.Index);
+        });
+
+        Comparer<Task> processingTimeThenIndex = Comparer<Task>
+        .Create((t1, t2) => {
+            if (t1.ProcessingTime != t2.ProcessingTime)
+            {
+                return t1.ProcessingTime.CompareTo(t2.ProcessingTime);
+            }
+            return t1.Index.CompareTo(t2.Index);
+        });  
+       
+
+        var  allTasks = new PriorityQueue<Task, Task>(enquethenProcessingTimeThenIndex);
+        var availableTasks = new PriorityQueue<Task, Task>(processingTimeThenIndex);
+        for (int i = 0, tasksLength = tasks.Length; i < tasksLength; i++)
+        {
+            int[] taskInfo = tasks[i];
+            Task task = new Task(taskInfo[0], taskInfo[1], i);
+            allTasks.Enqueue(task, task);
+        }
+        int virtualTime = 0;
+        int orderCounter = 0;
+        int[] order = new int[tasks.Length];
+        while (allTasks.Count > 0 || availableTasks.Count> 0)
+        {
+            Task nextTask;
+            if (availableTasks.Count == 0)
+            {
+                nextTask = allTasks.Dequeue();
+                virtualTime = nextTask.EnqueueTime;
+            }
+            else
+            {
+                nextTask = availableTasks.Dequeue();
+            }
+            order[orderCounter++] = nextTask.Index;
+            virtualTime += nextTask.ProcessingTime;
+            while (allTasks.Count >0 && allTasks.Peek().EnqueueTime <= virtualTime)
+            {
+                var taskTemp = allTasks.Dequeue();
+                availableTasks.Enqueue(taskTemp, taskTemp);
+            }
+        }
+        return order;
+    }
+
+    class Task
+    {
+        public int EnqueueTime;
+        public int ProcessingTime;
+        public int Index;
+
+        public Task(
+                 int enqueueTime,
+                 int processingTime,
+                 int index)
+        {
+            this.EnqueueTime = enqueueTime;
+            this.ProcessingTime = processingTime;
+            this.Index = index;
+        }
+
+    }
+    public static int[] GetOrder1(int[][] tasks)
+    {
+        var tasksTuples = new List<Tuple<int, int, int>>();
+        for (int i = 0; i < tasks.Length; i++)
+        {
+            tasksTuples.Add(new Tuple<int, int, int>(i, tasks[i][0], tasks[i][1]));
+        }
+        tasksTuples = tasksTuples.OrderBy(t => t.Item2).ToList();
+        var queue = new PriorityQueue<int,int>();
+        var res = new int[tasks.Length];
+        var j = 0;
+        var s = 0;
+        var currentTime = tasksTuples[0].Item2;
+        while (s < tasksTuples.Count)
+        {
+            if (currentTime >= tasksTuples[s].Item2 || queue.Count == 0)
+            {
+                queue.Enqueue(tasksTuples[s].Item1, tasksTuples[s++].Item3);
+            }
+            else
+            {
+                break;
+            }
+        }
+               // queue.Enqueue(index, tasks[index++][1]);
+        while (queue.TryDequeue(out var i, out var proTime ))
+        {
+            res[j++] = i;
+            currentTime += proTime;
+            while (s < tasksTuples.Count)
+            {
+                if (currentTime >= tasksTuples[s].Item2 || queue.Count == 0)
+                {
+                    queue.Enqueue(tasksTuples[s].Item1, tasksTuples[s++].Item3);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+
+
+
+
+}
