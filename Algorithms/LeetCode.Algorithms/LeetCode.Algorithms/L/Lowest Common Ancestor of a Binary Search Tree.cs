da84e92d08e646a8a796da586d055acfc9629a29
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-12-30 15:44:38 +0200

    235. Lowest Common Ancestor of a Binary Search Tree Leet Code #81

---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/L/Lowest Common Ancestor of a Binary Search Tree.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCode.Algorithms;
+
+public class Lowest_Common_Ancestor_of_a_Binary_Search_Tree
+{
+    public static TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    {
+        if (root.val > p.val && root.val > q.val) return LowestCommonAncestor(root.left, p, q);
+        else if (root.val < p.val && root.val < q.val) return LowestCommonAncestor(root.right, p, q);
+        else return root;
+    }
+}
