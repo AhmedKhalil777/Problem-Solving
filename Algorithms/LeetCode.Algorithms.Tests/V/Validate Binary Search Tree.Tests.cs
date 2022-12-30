c3c4a749f20983cc85683b95666e5cc54b8e46f4
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-12-30 15:30:23 +0200

    235. Lowest Common Ancestor of a Binary Search Tree

---
+++ Algorithms/LeetCode.Algorithms.Tests/V/Validate Binary Search Tree.Tests.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Algorithms.Tests;
+
+public class Validate_Binary_Search_Tree
+{
+    [Fact]
+    public void IsValidBST1()
+    {
+        var node = new TreeNode(2, 
+            new TreeNode(1),
+            new TreeNode (3));
+        Algorithms.Validate_Binary_Search_Tree.IsValidBST(node).Should().BeTrue();
+    }
+
+    [Fact]
+    public void IsValidBST2()
+    {
+        var node = new TreeNode(5,
+            new (1),
+            new (4, new (3), new(6)));
+        Algorithms.Validate_Binary_Search_Tree.IsValidBST(node).Should().BeFalse();
+    }
+
+    [Fact]
+    public void IsValidBST3()
+    {
+        var node = new TreeNode(2,
+            new(2),
+            new(2));
+        Algorithms.Validate_Binary_Search_Tree.IsValidBST(node).Should().BeFalse();
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/V/Validate Binary Search Tree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+public static class Validate_Binary_Search_Tree
+{
+    /// <summary>
+    /// Using BST PreOrder Traverse to check
+    /// </summary>
+    /// <param name="root"></param>
+    /// <returns></returns>
+    public static bool IsValidBST(TreeNode root)
+    {
+        return Validate(root, null, null);
+    }
+
+    public static bool Validate(TreeNode root, int? low, int? high)
+    {
+        if (root == null)
+        {
+            return true;
+        }
+        if ((low != null && root.val <= low) || (high != null && root.val >= high))
+        {
+            return false;
+        }
+        return Validate(root.right, root.val, high) && Validate(root.left, low, root.val);
+    }
+
+}
