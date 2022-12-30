ccdf88c45bb43ee579ca7a8cd8229d75a7b7b4cd
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-12-30 12:16:29 +0200

    797. All Paths From Source to Target Leet Code #79

---
+++ Algorithms/LeetCode.Algorithms.Tests/A/All Paths From Source to Target.Tests.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace LeetCode.Algorithms.Tests;
+
+[UsesVerify]
+public class All_Paths_From_Source_to_Target
+{
+    [Theory]
+    [InlineData("[[1,2],[3],[3],[]]", "Test1")]
+    [InlineData("[[4,3,1],[3,2,4],[3],[4],[]]","Test2")]
+    public async Task AllPathsSourceTarget(string json, string verifierFile)
+    {
+        var actual = JsonConvert.DeserializeObject<int[][]>( json);
+        await Verify(target: Algorithms.All_Paths_From_Source_to_Target.AllPathsSourceTarget(actual))
+            .UseTextForParameters(verifierFile);
+        
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/A/All Paths From Source to Target.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Xml.Linq;
+
+namespace LeetCode.Algorithms;
+
+public static  class All_Paths_From_Source_to_Target
+{
+
+    public static void DFS(List<IList<int>> result, List<int> currentPath, int currNode, int destination, int[][] graph)
+    {
+        var newCurrPath = new List<int>();
+        foreach (var node in currentPath) { newCurrPath.Add(node); }
+        newCurrPath.Add(currNode);
+        if (currNode == destination)
+        {
+            result.Add(newCurrPath);
+            return;
+        }
+        else
+        {
+            for (int i = 0; i < graph[currNode].Length; i++)
+            {
+                DFS(result, newCurrPath, graph[currNode][i], destination, graph);
+            }
+        }
+    }
+
+    public static IList<IList<int>> AllPathsSourceTarget(int[][] graph)
+    {
+        var result = new List<IList<int>>();
+        for (int i = 0; i < graph[0].Length; i++)
+        {
+            var currPath = new List<int>{ 0 };
+            DFS(result,currPath, graph[0][i], graph.Length - 1, graph);
+        }
+        return result;
+    }
+}
