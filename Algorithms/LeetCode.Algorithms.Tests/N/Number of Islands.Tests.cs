9b148e8e4695c5e44a4e5baa71717d76e6724150
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-12-31 20:57:48 +0200

    200. Number of Islands Leet Code #86

---
+++ Algorithms/LeetCode.Algorithms.Tests/N/Number of Islands.Tests.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace LeetCode.Algorithms.Tests;
+
+public class Number_of_Islands
+{
+    [Theory]
+    [InlineData("[\r\n  [\"1\",\"1\",\"1\",\"1\",\"0\"],\r\n  [\"1\",\"1\",\"0\",\"1\",\"0\"],\r\n  [\"1\",\"1\",\"0\",\"0\",\"0\"],\r\n  [\"0\",\"0\",\"0\",\"0\",\"0\"]\r\n]", 1)]
+    [InlineData("[\r\n  [\"1\",\"1\",\"0\",\"0\",\"0\"],\r\n  [\"1\",\"1\",\"0\",\"0\",\"0\"],\r\n  [\"0\",\"0\",\"1\",\"0\",\"0\"],\r\n  [\"0\",\"0\",\"0\",\"1\",\"1\"]\r\n]", 3)]
+    public void NumIslands(string actJson, int res)
+    {
+        var actual = JsonConvert.DeserializeObject<char[][]>(actJson);
+        Algorithms.Number_of_Islands.NumIslands(actual).Should().Be(res);
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/N/Number of Islands.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCode.Algorithms;
+
+public class Number_of_Islands
+{
+    public static List<Tuple<int, int>> Directions = new List<Tuple<int, int>>
+    {
+        new(1,0),
+        new(0,1),
+        new(-1,0),
+        new(0,-1),
+    };
+    public static int NumIslands(char[][] grid)
+    {
+        int m = grid.Length;
+        int n = grid[0].Length;
+        bool[,] visited = new bool[m, n];
+        int numIslands = 0;
+
+        for (int r = 0; r < m; r++)
+        {
+            for (int c = 0; c < n; c++)
+            {
+                if (visited[r, c] || grid[r][c] != '1')
+                    continue;
+                TryVisit(grid, r, c, visited, m, n);
+                numIslands++;
+            }
+        }
+
+        return numIslands;
+    }
+
+    public static void TryVisit(char[][] grid, int r, int c, bool[,] visited, int m, int n)
+    {
+        if (r < 0 || c < 0 || r >= m || c >= n || visited[r, c] || grid[r][c] != '1')
+        {
+            return;
+        }
+
+        visited[r, c] = true;
+
+        foreach (var (rD , cD) in Directions)
+        {
+            TryVisit(grid, r + rD, c + cD, visited, m, n);
+
+        }
+    }
+}
