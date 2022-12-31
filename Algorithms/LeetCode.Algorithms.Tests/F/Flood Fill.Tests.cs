227ba837bb6592d74c38d56338c0e86c9ade8ff3
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2022-12-31 18:20:30 +0200

    733. Flood Fill Leet Code #85

---
+++ Algorithms/LeetCode.Algorithms.Tests/F/Flood Fill.Tests.cs
@@ -0,0 +1,18 @@
+﻿
+using Newtonsoft.Json;
+
+namespace LeetCode.Algorithms.Tests;
+
+[UsesVerify]
+public class Flood_Fill
+{
+    [Theory]
+    [InlineData("[[1,1,1],[1,1,0],[1,0,1]]", 1,1,2,"ExampleI")]
+    [InlineData("[[0,0,0],[0,0,0]]", 0,0,0,"Example2")]
+    public async Task FloodFill(string imageJsonAct ,int sr, int sc, int color, string testVerificationName)
+    {
+        var actual = JsonConvert.DeserializeObject<int[][]>(imageJsonAct);
+        await Verify(Algorithms.Flood_Fill.FloodFill(actual, sr, sc, color))
+            .UseTextForParameters(testVerificationName);
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/F/Flood Fill.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode.Algorithms;
+
+public static class Flood_Fill
+{
+    public static List<Tuple<int, int>> Directions = new List<Tuple<int, int>>
+    {
+        new(1,0),
+        new(0,1),
+        new(-1,0),
+        new(0,-1),
+    };
+    public static int[][] FloodFill(int[][] image, int sr, int sc, int color)
+    {
+        var prevColor = image[sr][sc];
+        if (prevColor == color)
+        {
+            return image;
+        }
+
+        Color( image, sr, sc, prevColor, color);
+        return image;
+    }
+
+    public static void Color(int[][] image, int sr, int sc, in int prevColor,int color)
+    {
+        if (sr < 0 || sr >= image.Length || sc <0 || sc>= image[0].Length )
+        {
+            return;
+        }
+        if (prevColor != image[sr][sc] )
+        {
+            return;
+        }
+        image[sr][sc] = color;
+        
+        foreach (var dir in Directions)
+        {
+            Color( image, sr + dir.Item1, sc + dir.Item2, prevColor, color);
+
+        }
+    }
+}
