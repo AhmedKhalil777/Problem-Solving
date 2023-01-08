0c2c8dbf17419c515567cecc3531b147bf2687d8
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2023-01-08 10:29:15 +0200

    1833. Maximum Ice Cream Bars Leet Code #87

---
+++ Algorithms/LeetCode.Algorithms.Tests/M/Maximum Ice Cream Bars.Tests.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace LeetCode.Algorithms.Tests;
+
+public class Maximum_Ice_Cream_Bars
+{
+    [Theory]
+    [InlineData("[1,3,2,4,1]",7, 4)]
+    [InlineData("[1,3,2,4,1]", 5, 3)]
+    [InlineData("[1,6,3,1,2,5]", 20, 6)]
+    public void MaxIceCream(string actJson, int coins ,int res)
+    {
+        var actualCosts = JsonConvert.DeserializeObject<int[]>(actJson);
+        Algorithms.Maximum_Ice_Cream_Bars.MaxIceCream(actualCosts, coins)
+            .Should().Be(res);
+        Algorithms.Maximum_Ice_Cream_Bars.MaxIceCreamM1(actualCosts, coins)
+            .Should().Be(res);
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/M/Maximum Ice Cream Bars.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Scripting.Runtime;
+
+namespace LeetCode.Algorithms;
+
+public class Maximum_Ice_Cream_Bars
+{
+
+    public static int MaxIceCream(int[] costs, int coins)
+    {
+        var max = costs.Max(x => x);
+        var freqMatrix = new int[max + 1];
+        var res = 0;
+        foreach (var cost in costs)
+        {
+            freqMatrix[cost] = freqMatrix[cost] +1;
+        }
+        for (int i = 1; i < freqMatrix.Length; i++)
+        {
+            if (freqMatrix[i] == 0)
+            {
+                continue;
+            }
+            if(coins < i)
+            {
+                break;
+            }
+            int count = Math.Min(freqMatrix[i], coins / i);
+            coins -= i * count;
+            res += count;
+
+        }
+        return res;
+    }
+    public static int MaxIceCreamM1(int[] costs, int coins)
+    {
+        Array.Sort(costs);
+        var res = 0;
+        for (int i = 0; i < costs.Length; i++)
+        {
+            if (coins < costs[i])
+            {
+                return res;
+            }
+            coins -= costs[i];
+            res++;
+        }
+        return res;
+    }
+}
