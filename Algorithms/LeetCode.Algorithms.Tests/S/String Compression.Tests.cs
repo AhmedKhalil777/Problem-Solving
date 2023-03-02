74de82ab01685e751884b7d7ff09d06ed02d3308
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2023-03-02 23:38:11 +0200

    443. String Compression Leet Code #88

---
+++ Algorithms/LeetCode.Algorithms.Tests/S/String Compression.Tests.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Algorithms.Tests;
+
+public class String_Compression
+{
+
+    [Theory]
+    [InlineData(new char[] {'a','a','b','b','c','c','c'}, 6)]
+    [InlineData(new char[] {'a'}, 1)]
+    [InlineData(new char[] {'a','b','b','b','b','b','b','b','b','b','b','b','b'}, 4)]
+    public void Compress(char[] chars, int count)
+    {
+        var stringCompression = new Algorithms.String_Compression();
+        stringCompression.Compress(chars).Should().Be(count);
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/S/String Compression.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+
+namespace LeetCode.Algorithms;
+
+public class String_Compression
+{
+    public int Compress(char[] chars)
+    {
+        switch (chars.Length)
+        {
+            case 0:
+                return 0;
+            case 1:
+                return 1;
+        }
+
+        var prevChar = chars[0];
+        var charCount = 1;
+
+        var index = 0;
+        var total = 0;
+        for (int i = 1; i < chars.Length +1 ; i++)
+        {
+            
+            if (i == chars.Length || chars[i] != prevChar )
+            {
+                if (charCount == 1)
+                {
+                    chars[index] = prevChar;
+                    total++;
+                    index++;
+                }
+                else
+                {
+                    var str = prevChar.ToString() + charCount;
+                    foreach (var ch in str)
+                    {
+                        chars[index] = ch;
+                        total++;
+                        index++;
+                    }
+                }
+                if (i == chars.Length)
+                {
+                    break;
+                }
+                charCount = 0;
+
+            }
+            charCount++;
+            prevChar = chars[i];
+        }
+
+        return total;
+    }
+}
