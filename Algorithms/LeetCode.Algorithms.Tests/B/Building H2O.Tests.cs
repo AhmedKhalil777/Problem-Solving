1bc8789647813860474ec4017025f486f3bec786
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2023-03-04 12:10:23 +0200

    1117. Building H2O Leet Code #89

---
+++ Algorithms/LeetCode.Algorithms.Tests/B/Building H2O.Tests.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+namespace LeetCode.Algorithms;
+
+public class Building_H2O
+{
+    [Theory]
+    [InlineData("HHOHHOHHOHHOHHOHHOHHOHHOHHOHHOHHOHHOHHOHHOHHOHHOHHOHHOHHOHHOHHOHHOHHOHHOHHOHHOHHOHHOHHOHHOHHOHHO")]
+    public void H2OTest(string atomsStream)
+    {
+        atomsStream = string.Concat(atomsStream.ToCharArray().OrderBy(x => Guid.NewGuid()));
+        var h2oGenerator = new H2OCreator();
+        var str = new StringBuilder();
+        Parallel.ForEach(atomsStream, c =>
+        {
+            if (c == 'H')
+            {
+                h2oGenerator.Hydrogen(() => str.Append('H'));
+            }
+            else
+            {
+                h2oGenerator.Oxygen(() => str.Append('O'));
+
+            }
+        });
+        var res = str.ToString();
+        Trace.WriteLine(res);
+        for (int i = 0; i < res.Length; i+= 3)
+        {
+           var h2o= res.Skip(i).Take(3);
+           h2o.Count(x => x == 'H').Should().Be(2);
+           h2o.Count(x => x == 'O').Should().Be(1);
+        }
+    }
+}
+
+
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/B/Building H2O.cs
@@ -0,0 +1,87 @@
+﻿namespace LeetCode.Algorithms;
+
+public class H2O
+{
+    private static object syncRoot = new();
+    private long hydrogens;
+    private long oxygens;
+
+    private static bool decrement = false;
+    public H2O()
+    {
+    }
+
+    public void Hydrogen(Action releaseHydrogen)
+    {
+        while (Interlocked.Read(ref hydrogens) >= 2 || decrement) 
+            Thread.Yield();
+
+
+        Interlocked.Increment(ref hydrogens);
+
+
+        // releaseHydrogen() outputs "H". Do not change or remove this line.
+        releaseHydrogen();
+    }
+
+    public void Oxygen(Action releaseOxygen)
+    {
+        while (Interlocked.Read(ref hydrogens) < 2 || decrement)
+            Thread.Yield();
+
+        
+        Interlocked.Increment(ref oxygens);
+
+
+        // releaseOxygen() outputs "O". Do not change or remove this line.
+        releaseOxygen();
+
+        // Decrement H20
+        lock (syncRoot)
+        {
+            decrement = true;
+            Interlocked.Decrement(ref oxygens);
+            Interlocked.Decrement(ref hydrogens);
+            Interlocked.Decrement(ref hydrogens);
+            decrement = false;
+        } 
+        
+
+    }
+}
+
+
+public class H2OCreator {
+    private readonly Semaphore H_to_O = new(0, 2);
+    private readonly Semaphore O_to_H = new (2, 2);
+
+    private readonly object syncLock = new ();
+    private readonly object semLock = new ();
+
+    public H2OCreator() {
+    }
+
+    public void Hydrogen(Action releaseHydrogen) {
+        
+        O_to_H.WaitOne();
+
+        lock (syncLock) {
+            // releaseHydrogen() outputs "H". Do not change or remove this line.
+            releaseHydrogen();
+        }
+
+        H_to_O.Release();
+    }
+
+    public void Oxygen(Action releaseOxygen) {
+        lock (semLock) {
+            H_to_O.WaitOne();
+            H_to_O.WaitOne();
+        }
+
+        // releaseOxygen() outputs "O". Do not change or remove this line.
+        releaseOxygen();
+
+        O_to_H.Release(2);
+    }
+}
