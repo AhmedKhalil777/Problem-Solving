e75ffea68d510cc17a96f6daf7e2304491ee1264
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2023-03-04 22:51:58 +0200

    Count Subarrays With Fixed Bounds Leet Code #90

---
+++ Algorithms/LeetCode.Algorithms.Tests/R/Regular Expression Matching.Tests.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Algorithms.Tests;
+
+public class Regular_Expression_Matching
+{
+    [Theory]
+    [InlineData("aa", "a", false)]
+    [InlineData("aa", "a*", true)]
+    [InlineData("ab", ".*", true)]
+    public void IsMatch(string s, string p, bool res)
+    {
+        new Algorithms.Regular_Expression_Matching()
+            .IsMatch(s, p)
+            .Should()
+            .Be(res);
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/R/Regular Expression Matching.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Algorithms;
+
+public class Regular_Expression_Matching
+{
+    public bool IsMatch(string s, string p)
+    {
+        if (string.IsNullOrEmpty(p)) return string.IsNullOrEmpty(s);
+        var firstMatch = (!string.IsNullOrEmpty(s) &&
+                            (p[0] == s[0] || p[0] == '.'));
+
+        if (p.Length >= 2 && p[1] == '*')
+        {
+            return (IsMatch(s, p[2..]) ||
+                    (firstMatch && IsMatch(s[1..], p)));
+        }
+
+        return firstMatch && IsMatch(s[1..], p[1..]);
+    }
+}
