4aa2d6eff924dc5ca8a1ae54ffbb99e194c04fc9
Ahmed777Khalil <progeng_ahmed_khalil@outlook.com>
2023-03-12 23:11:47 +0200

    11. Container With Most Water #93

---
+++ Algorithms/LeetCode.Algorithms.Tests/C/Container With Most Water.Tetst.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace LeetCode.Algorithms.Tests;
+
+public class Container_With_Most_Water
+{
+    [Theory]
+    [InlineData("[1,1]", 1)]
+    [InlineData("[1,8,6,2,5,4,8,3,7]", 49)]
+    public void MaxArea(string json, int res)
+    {
+        var arr = JsonSerializer.Deserialize<int[]>(json);
+        new Algorithms.Container_With_Most_Water().MaxArea(arr).Should().Be(res);
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/C/Container With Most Water.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Algorithms;
+
+public class Container_With_Most_Water
+{
+    public int MaxArea(int[] height) {
+        if (!height.Any() || height.Length == 1)
+        {
+            return 0;
+        }
+
+        var max = 0;
+        for (int i = 0, j = height.Length -1 ; i < j;)
+        {
+            if (height[i] > height[j])
+            {
+                var curr = (j - i) * height[j];
+                max = Math.Max(max, curr);
+                j--;
+            }
+            else
+            {
+                var curr = (j - i) * height[i];
+                max = Math.Max(max, curr);
+                i++;
+            }
+        }
+
+        return max;
+    }
+}
