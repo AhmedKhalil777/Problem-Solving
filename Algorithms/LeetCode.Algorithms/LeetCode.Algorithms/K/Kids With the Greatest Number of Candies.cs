82b7a48f2a121944055e9ee01197c7f178539191
Ahmed Khalil <progeng_ahmed_khalil@outlook.com>
2023-04-17 02:37:19 +0200

    1431. Kids With the Greatest Number of Candies Leet Code #97

---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/K/Kids With the Greatest Number of Candies.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms;
+
+public class Kids_With_the_Greatest_Number_of_Candies
+{
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+        var maxAdjusted = candies.Max() - extraCandies;
+        return candies.Select(_ => _  >= maxAdjusted).ToList();
+    }
+}
