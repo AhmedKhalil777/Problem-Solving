aa68152ed24ac1072ca6c9c652c8da8e406b5c36
Ahmed Khalil <progeng_ahmed_khalil@outlook.com>
2023-09-22 21:43:31 +0300

    1750. Minimum Length of String After Deleting Similar Ends Leet Code #107

---
+++ Algorithms/LeetCode.Algorithms.Tests/M/Minimum Length of String After Deleting Similar Ends.Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms.Tests;
+
+public class Minimum_Length_of_String_After_Deleting_Similar_Ends
+{
+    [Theory(DisplayName = "Minimum Length should return Minimum Length of String After Deleting Similar Ends")]
+    [InlineData("ca",2)]
+    [InlineData("cabaabac", 0)]
+    [InlineData("aabccabba", 3)]
+    [InlineData("bbbbbbbbbbbbbbbbbbbbbbbbbbbabbbbbbbbbbbbbbbccbcbcbccbbabbb", 1)]
+    [InlineData("bbbbbbbbbbbbbbbbbbb", 0)]
+
+    public void MinimumLength(string s, int res)
+    {
+        new Algorithms.Minimum_Length_of_String_After_Deleting_Similar_Ends().MinimumLength(s).Should().Be(res);
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/M/Minimum Length of String After Deleting Similar Ends.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Algorithms;
+
+public class Minimum_Length_of_String_After_Deleting_Similar_Ends
+{
+    public int MinimumLength(string s)
+    {
+        if (string.IsNullOrEmpty(s)) return 0;
+        int i = 0, j = s.Length - 1;
+        while (j - i > 0)
+        {
+            var c = s[i];
+            if (s[j] != c)
+            {
+                return j - i +1;
+            }
+            while (i< s.Length && s[i] == c)
+            {
+                i++;
+            }
+            while (j > 0 && s[j] == c)
+            {
+                j--;
+            }
+        }
+        if (j-i == 0)
+        {
+            return 1;
+        }
+
+        return 0;
+    }
+}
