32476e2f4dc49d3561278811cb07d7bb8971f291
Ahmed Khalil <progeng_ahmed_khalil@outlook.com>
2023-09-22 20:48:12 +0300

    1768. Merge Strings Alternately Leet Code #106

---
+++ Algorithms/LeetCode.Algorithms.Tests/M/Merge Strings Alternately.Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms.Tests;
+
+public class Merge_Strings_Alternately
+{
+    [Theory]
+    [InlineData("abc", "pqr", "apbqcr")]
+    [InlineData("ab", "pqrs", "apbqrs")]
+    public void MergeAlternately(string word1, string word2, string res)
+    {
+        new Algorithms.Merge_Strings_Alternately().MergeAlternately(word1, word2).Should().Be(res);
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/M/Merge Strings Alternately.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace LeetCode.Algorithms;
+
+public class Merge_Strings_Alternately
+{
+    public string MergeAlternately(string word1, string word2)
+    {
+        if (word1.Length == 0)
+        {
+            return word2;
+        }
+        if (word2.Length ==0)
+        {
+            return word1;
+        }
+        var sb = new StringBuilder();
+        var minLength = word1.Length < word2.Length ? word1.Length : word2.Length;
+        for (int i = 0; i < minLength; i++)
+        {
+
+            sb.Append(word1[i]);
+            sb.Append(word2[i]);
+            if (i+1 == word1.Length)
+            {
+                sb.Append(word2[(i+1)..(word2.Length)]);
+            }
+            if (i + 1 == word2.Length)
+            {
+                sb.Append(word1[(i+1)..(word1.Length)]);
+            }
+        }
+        return sb.ToString();   
+    }
+}
