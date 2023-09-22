3ddfd54b0a6bcf9df51a6e9b140688435fd4a6a7
Ahmed Khalil <progeng_ahmed_khalil@outlook.com>
2023-09-22 21:48:53 +0300

    1688. Count of Matches in Tournament Leet Code #108

---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/N/Count of Matches in Tournament.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms;
+
+public class Count_of_Matches_in_Tournament
+{
+    public int NumberOfMatches(int n) => --n;
+
+}
