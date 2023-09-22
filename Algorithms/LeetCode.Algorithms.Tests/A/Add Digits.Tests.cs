44603894830f1379b560154920e7c5ab755f9477
Ahmed Khalil <progeng_ahmed_khalil@outlook.com>
2023-09-22 20:25:23 +0300

    258. Add Digits Leet Code #105

---
+++ Algorithms/LeetCode.Algorithms.Tests/A/Add Digits.Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms.Tests;
+
+public class Add_Digits
+{
+    [Theory(DisplayName = "Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.")]
+    [InlineData(38,2)]
+    [InlineData(0,0)]
+    public void AddDigits(int num, int res)
+    {
+        new Algorithms.Add_Digits().AddDigits(num).Should().Be(res);
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/A/Add Digits.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Algorithms;
+
+public class Add_Digits
+{
+
+    public int AddDigits(int num)
+    {
+        var numStr = num.ToString();
+        if (numStr.Length == 1)
+        {
+            return num;
+        }
+
+        return AddDigits(numStr.Select(c => int.Parse($"{c}")).Sum());
+    }
+}
