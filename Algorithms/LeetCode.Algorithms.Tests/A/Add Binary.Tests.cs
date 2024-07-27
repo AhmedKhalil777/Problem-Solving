08c775f460171437fc75ad601a0fd9a31c1f417e
Ahmed Khalil <progeng_ahmed_khalil@outlook.com>
2024-07-27 15:31:07 +0300

    67. Add Binary #111

---
+++ Algorithms/LeetCode.Algorithms.Tests/A/Add Binary.Tests.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Algorithms.Tests;
+
+public class Add_Binary
+{
+    [Theory]
+    [InlineData("11", "1", "100" )]
+    [InlineData("1010", "1011", "10101" )]
+    [InlineData("1", "111", "1000" )]
+    [InlineData("101111", "10", "110001")]
+    public void AddBinary(string a, string b, string expected)
+    {
+        new Algorithms.Add_Binary().AddBinary(a, b).Should().Be(expected);
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/A/Add Binary.cs
@@ -0,0 +1,74 @@
+﻿using System.Text;
+
+namespace LeetCode.Algorithms;
+
+public class Add_Binary
+{
+    public string AddBinary(string a, string b)
+    {
+        var builder = new StringBuilder();
+        var max = (a.Length > b.Length ? a : b).Reverse().ToList();
+        var min = (b.Length >= a.Length ? a : b).Reverse().ToList();
+        var hand = 0;
+        for (var i = 0; i < max.Count; i++)
+        {
+            if (i >= min.Count)
+            {
+                if (max[i] == '1' && hand == 1)
+                {
+                    builder.Append('0');
+                }
+                else if(hand == 1)
+                {
+                    builder.Append('1');
+                    hand = 0;
+                }
+                else
+                {
+                    builder.Append(max[i]);
+                }
+
+                continue;
+            }
+            if (max[i] == '1' && min[i] == '1')
+            {
+                if (hand == 1)
+                {
+                    builder.Append('1');
+                }
+                else
+                    builder.Append('0');
+                hand = 1;
+
+            }
+            else if (max[i] == '1' || min[i] == '1')
+            {
+                if (hand == 1)
+                {
+                    builder.Append('0');
+                }
+                else
+                {
+                    builder.Append('1');
+                }
+            }
+            else
+            {
+                if (hand == 1)
+                {
+                    builder.Append('1');
+                    hand = 0;
+                }
+                else
+                {
+                    builder.Append('0');
+                }
+            }
+        }
+
+        if (hand == 1)
+            builder.Append('1');
+
+        return string.Join( "",  builder.ToString().Reverse());
+    }
+}
