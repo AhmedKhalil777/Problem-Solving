2e0fdcf7b1f7d9c78c095008d18159379336188a
Ahmed Khalil <progeng_ahmed_khalil@outlook.com>
2024-07-27 12:01:22 +0300

    Three Sum Closest #109

---
+++ Algorithms/LeetCode.Algorithms.Tests/T/ThreeSumClosest.Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms.Tests;
+
+public class ThreeSumClosestClass
+{
+    [Theory]
+    [InlineData("[-1,2,1,-4]", 1,2)]
+    [InlineData("[0,0,0]", 1,0)]
+    public void ThreeSumClosest(string numsArrayJson, int target ,int expected)
+    {
+        var arr = JsonSerializer.Deserialize<int[]>(numsArrayJson);
+        new Algorithms.ThreeSumClosestClass().ThreeSumClosest(arr, target).Should().Be(expected);
+    }
+}
---
+++ Algorithms/LeetCode.Algorithms/LeetCode.Algorithms/T/ThreeSumClosest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms;
+
+public class ThreeSumClosestClass
+{
+    public int ThreeSumClosest(int[] nums, int target)
+    {
+        if (nums.Length < 3)
+            return 0;
+
+        Array.Sort(nums);
+        int start = 0;
+        int left = 1;
+        int right = nums.Length - 1;
+        int minDistance = int.MaxValue;
+        int sum = int.MinValue;
+
+        while (start < nums.Length - 2)
+        {
+            while (left < right)
+            {
+                int currSum = nums[start] + nums[left] + nums[right];
+                if (currSum == target)
+                    return target;
+
+                if (currSum < target)
+                    left++;
+                else
+                    right--;
+
+                if (Math.Abs(currSum - target) < minDistance)
+                {
+                    sum = currSum;
+                    minDistance = Math.Abs(currSum - target);
+                }
+            }
+            start++;
+            left = start + 1;
+            right = nums.Length - 1;
+        }
+        return sum;
+    }
+}
